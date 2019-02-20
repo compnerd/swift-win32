e6e408760df190969edfd92f0cfedb8de5466fa3
Saleem Abdulrasool <compnerd@compnerd.org>
2019-02-19 22:42:41 -0800

    HelloSwift: implement the window delegate

--- HelloSwift.swift
+++ HelloSwift.swift
@@ -6,7 +6,14 @@ let SwiftWindowClass: WindowClass =
     WindowClass(hInst: GetModuleHandleW(nil), name: "Swift.Window")
 _ = SwiftWindowClass.register()
 
-let window = Window(class: SwiftWindowClass, title: "Swift Window")
+class SwiftWindowDelegate: WindowDelegate {
+  func OnDestroy(_: HWND, _: WPARAM, _: LPARAM) -> LRESULT {
+    exit(EXIT_SUCCESS)
+  }
+}
+
+var window = Window(class: SwiftWindowClass, title: "Swift Window")
+window.delegate = SwiftWindowDelegate()
 window.show()
 window._runMessageLoop()
 
