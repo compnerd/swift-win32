3fa460a3a2ddd399bb59f87c809af6bc1cc960cd
Saleem Abdulrasool <compnerd@compnerd.org>
2019-02-21 22:35:14 -0800

    HelloSwift: use the ApplicationDelegate
    Update the HelloSwift example to use the ApplicationDelegate to manage
    the lifecycle.  This should eventually move the window to the
    Application and have the delegate create the application window.

--- HelloSwift.swift
+++ HelloSwift.swift
@@ -2,19 +2,30 @@
 import WinSDK
 import SwiftWin32
 
-let SwiftWindowClass: WindowClass =
-    WindowClass(hInst: GetModuleHandleW(nil), name: "Swift.Window")
-_ = SwiftWindowClass.register()
+class SwiftApplicationDelegate: ApplicationDelegate {
+  var window: Window?
 
-class SwiftWindowDelegate: WindowDelegate {
-  func OnDestroy(_: HWND, _: WPARAM, _: LPARAM) -> LRESULT {
-    exit(EXIT_SUCCESS)
+  func application(_: Application,
+                   didFinishLaunchingWithOptions options: [Application.LaunchOptionsKey:Any]?) -> Bool {
+    let SwiftWindowClass: WindowClass =
+    WindowClass(hInst: GetModuleHandleW(nil), name: "Swift.Window")
+    _ = SwiftWindowClass.register()
+
+    class SwiftWindowDelegate: WindowDelegate {
+      func OnDestroy(_: HWND, _: WPARAM, _: LPARAM) -> LRESULT {
+        exit(EXIT_SUCCESS)
+      }
+    }
+
+    self.window = Window(class: SwiftWindowClass, title: "Swift Window")
+    self.window?.delegate = SwiftWindowDelegate()
+    self.window?.show()
+
+    return true
   }
 }
 
-var window = Window(class: SwiftWindowClass, title: "Swift Window")
-window.delegate = SwiftWindowDelegate()
-window.show()
 
-_ = ApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, nil)
+_ = ApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil,
+                    SwiftApplicationDelegate())
 
