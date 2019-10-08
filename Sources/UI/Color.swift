01ffa4a7acaca7b84e841e465c318a015c7234cc
Saleem Abdulrasool <compnerd@compnerd.org>
2019-10-08 10:19:29 -0700

    Color: add some default color values

--- Sources/UI/Color.swift
+++ Sources/UI/Color.swift
@@ -27,3 +27,15 @@ public func ==(lhs: Color, rhs: Color) -> Bool {
   return lhs.rawValue == rhs.rawValue
 }
 
+extension Color {
+  static class var blue: Color = Color(red: 0.0, green: 0.0, blue: 1.0)
+  static class var brown: Color = Color(red: 0.6, green: 0.4, blue: 0.2)
+  static class var cyan: Color = Color(red: 0.0, green: 1.0, blue: 1.0)
+  static class var green: Color = Color(red: 0.0, green: 1.0, blue: 0.0)
+  static class var magenta: Color = Color(red: 1.0, green: 0.0, blue: 1.0)
+  static class var orange: Color = Color(red: 1.0, green: 0.5, blue: 0.0)
+  static class var purple: Color = Color(red: 0.5, green: 0.0, blue: 0.5)
+  static class var red: Color = Color(red: 1.0, green: 0.0, blue: 0.0)
+  static class var yellow: Color = Color(red: 1.0, green: 1.0, blue: 0.0)
+}
+
