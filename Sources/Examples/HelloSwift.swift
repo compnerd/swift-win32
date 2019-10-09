2b707e5f3084e3842fd37e0e51fda9d791c889a8
Saleem Abdulrasool <compnerd@compnerd.org>
2019-10-08 22:39:19 -0700

    Examples: update code for LLVM demo

--- Sources/Examples/HelloSwift.swift
+++ Sources/Examples/HelloSwift.swift
@@ -1,29 +1,31 @@
 import WinSDK
 import SwiftWin32
 
-class SwiftApplicationDelegate: ApplicationDelegate {
-  lazy var window: Window = Window(title: "Swift/Win32 Window")
-  lazy var button: Button = Button(frame: Rect(x: 10, y: 10, width: 0, height: 0),
-                                   title: "Press Me!")
-
-  func application(_: Application,
-                   didFinishLaunchingWithOptions options: [Application.LaunchOptionsKey:Any]?) -> Bool {
-    window.addSubview(self.button)
-    window.delegate = self
-    return true
-  }
-}
-
-extension SwiftApplicationDelegate: WindowDelegate {
-  func OnDestroy(_ hWnd: HWND?, _ wParam: WPARAM, _ lParam: LPARAM) -> LRESULT {
+class EventHandler: WindowDelegate {
+  func OnDestroy(_ hWnd: HWND?, _ wParam: WPARAM, _ lParam: LPARAM)
+      -> LRESULT {
     PostQuitMessage(0)
     return 0
   }
 
-  func OnCommand(_ hWnd: HWND?, _ wParam: WPARAM, _ lParam: LPARAM) -> LRESULT {
+  func OnCommand(_ hWnd: HWND?, _ wParam: WPARAM, _ lParam: LPARAM)
+      -> LRESULT {
     MessageBoxW(nil, "Swift/Win32 Demo!".LPCWSTR,
                 "Swift/Win32 MessageBox!".LPCWSTR, UINT(MB_OK))
     return 0
+  }
+}
+
+class SwiftApplicationDelegate: ApplicationDelegate {
+  lazy var window = Window(title: "Swift/Win32 Window")
+  lazy var button = Button(frame: .zero, title: "Press Me!")
+  lazy var delegate = EventHandler()
+
+  func application(_: Application,
+                   didFinishLaunchingWithOptions options: [Application.LaunchOptionsKey:Any]?) -> Bool {
+    window.addSubview(self.button)
+    window.delegate = delegate
+    return true
   }
 }
 
