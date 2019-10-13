3c235ca0284fcd83f32f6cb0236dbcaf1ecc0944
Saleem Abdulrasool <compnerd@compnerd.org>
2019-10-13 15:35:54 -0700

    Label: correct Window class

--- Sources/UI/Label.swift
+++ Sources/UI/Label.swift
@@ -2,8 +2,7 @@
 import WinSDK
 
 public class Label: View {
-  public static let `class`: WindowClass =
-      WindowClass(hInst: GetModuleHandleW(nil), name: "Swift.Label")
+  public static let `class`: WindowClass = WindowClass(named: "STATIC")
 
   public override init(frame: Rect, `class`: WindowClass = Label.class,
                        style: DWORD = DWORD(WS_TABSTOP | WS_VISIBLE)) {
