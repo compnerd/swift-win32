c02f052f3b0d7806ba94081a2b900f3ebcec2167
Saleem Abdulrasool <compnerd@compnerd.org>
2019-10-24 18:02:47 -0700

    UI: add EdgeInsets

---
+++ Sources/UI/EdgeInsets.swift
@@ -0,0 +1,60 @@
+/**
+ * Copyright © 2019 Saleem Abdulrasool <compnerd@compnerd.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+public class EdgeInsets {
+  public static let zero: EdgeInsets = EdgeInsets()
+
+  public let bottom: Double
+  public let left: Double
+  public let right: Double
+  public let top: Double
+
+  public init() {
+    self.bottom = 0.0
+    self.left = 0.0
+    self.right = 0.0
+    self.top = 0.0
+  }
+
+  public init(top: Double, left: Double, bottom: Double, right: Double) {
+    self.bottom = bottom
+    self.left = left
+    self.right = right
+    self.top = top
+  }
+}
+
+public extension EdgeInsets: Equatable {
+  public static func == (_ lhs: EdgeInsets, _ rhs: EdgeInsets) {
+    return lhs.bottom == rhs.bottom &&
+           lhs.left == rhs.left &&
+           lhs.right == rhs.right &&
+           lhs.top == rhs.top
+  }
+}
