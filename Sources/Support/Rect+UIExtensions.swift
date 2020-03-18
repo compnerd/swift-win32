73da3b0959dd37f3866af6b99573e5d051483916
Saleem Abdulrasool <compnerd@compnerd.org>
2020-03-17 22:01:50 -0700

    Support: correct visibility on Rect extension
    The particular value is heavily used in constructor default values.
    Make the extension containing the property public.

--- Sources/Support/Rect+UIExtensions.swift
+++ Sources/Support/Rect+UIExtensions.swift
@@ -29,8 +29,8 @@
 
 import WinSDK
 
-internal extension Rect {
-  public static let `default`: Rect =
+public extension Rect {
+  static let `default`: Rect =
       Rect(x: Double(CW_USEDEFAULT), y: Double(CW_USEDEFAULT),
            width: Double(CW_USEDEFAULT), height: Double(CW_USEDEFAULT))
 }
