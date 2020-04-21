d3c1cb9873230b666b89ac6b0121fc1532f2777c
Saleem Abdulrasool <compnerd@compnerd.org>
2020-04-21 11:27:11 -0700

    Update WinSDK+Extensions.swift
    Correct version check.

--- Sources/Support/WinSDK+Extensions.swift
+++ Sources/Support/WinSDK+Extensions.swift
@@ -32,7 +32,7 @@ import WinSDK
 internal let IDC_ARROW: UnsafePointer<WCHAR> =
     UnsafePointer<WCHAR>(bitPattern: 32512)!
 
-#if swift(<=5.3)
+#if swift(<5.3)
 // windef.h
 internal let DPI_AWARENESS_CONTEXT_UNAWARE: DPI_AWARENESS_CONTEXT =
     DPI_AWARENESS_CONTEXT(bitPattern: -1)!
