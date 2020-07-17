4c480874b2034c8b3354b51ec9d9afe652712235
Saleem Abdulrasool <compnerd@compnerd.org>
2020-07-16 19:49:26 -0700

    UI: remove `OnDestroy` from `WindowDelegate`
    There is no need to expose the `OnDestroy` event to the user.  This is
    handled properly by default.  The only need is to post the quit message
    (`WM_QUIT`) when the window is destroyed to terminate the app.

--- Examples/HelloSwift.swift
+++ Examples/HelloSwift.swift
@@ -31,12 +31,6 @@ import WinSDK
 import SwiftWin32
 
 class EventHandler: WindowDelegate {
-  func OnDestroy(_ hWnd: HWND?, _ wParam: WPARAM, _ lParam: LPARAM)
-      -> LRESULT {
-    PostQuitMessage(0)
-    return 0
-  }
-
   func OnCommand(_ hWnd: HWND?, _ wParam: WPARAM, _ lParam: LPARAM)
       -> LRESULT {
     MessageBoxW(nil, "Swift/Win32 Demo!".LPCWSTR,
