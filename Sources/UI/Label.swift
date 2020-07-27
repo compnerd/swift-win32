694122713012fddd4472335001e05c95f2f59629
Saleem Abdulrasool <compnerd@compnerd.org>
2020-07-26 19:07:12 -0700

    UI: add backing store for Font
    We would immediately drop the Font which would free the font.  This
    resulted in the font setting never taking effect.  This is the same
    issue as the `TextField`.  Replicate the fix to `Label` and `TextView`.

--- Sources/UI/Label.swift
+++ Sources/UI/Label.swift
@@ -34,14 +34,17 @@ public class Label: Control {
   internal static let style: WindowStyle =
       (base: DWORD(WS_TABSTOP | WS_VISIBLE), extended: 0)
 
+  private var _font: Font?
   public var font: Font! {
     get {
+      guard _font == nil else { return _font }
       let lResult: LRESULT = SendMessageW(hWnd, UINT(WM_GETFONT), 0, 0)
       return Font(FontHandle(referencing: HFONT(bitPattern: Int(lResult))))
     }
     set(font) {
+      self._font = font
       SendMessageW(hWnd, UINT(WM_SETFONT),
-                   unsafeBitCast(font.hFont.value, to: WPARAM.self), LPARAM(1))
+                   unsafeBitCast(font?.hFont.value, to: WPARAM.self), LPARAM(1))
     }
   }
 
