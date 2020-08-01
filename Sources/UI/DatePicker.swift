6b7ca160a96111b879528f85f3fe4ddeacc5900d
Saleem Abdulrasool <compnerd@compnerd.org>
2020-07-31 20:55:37 -0700

    UI: sketch out DatePicker
    Add a basic date picker UI element.  This only renders the widget but
    does not allow you to query the value or change the value
    programatically.

---
+++ Sources/UI/DatePicker.swift
@@ -0,0 +1,63 @@
+/**
+ * Copyright © 2020 Saleem Abdulrasool <compnerd@compnerd.org>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO
+ * EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ **/
+
+import WinSDK
+
+internal let SwiftDatePickerProc: SUBCLASSPROC = { (hWnd, uMsg, wParam, lParam, uIdSubclass, dwRefData) in
+  let datepicker: DatePicker? =
+      unsafeBitCast(dwRefData, to: AnyObject.self) as? DatePicker
+  return DefSubclassProc(hWnd, uMsg, wParam, lParam)
+}
+
+public enum DatePickerStyle: Int {
+case automatic
+case wheels
+case compact
+case inline
+}
+
+public class DatePicker: Control {
+  internal static let `class`: WindowClass =
+      WindowClass(named: DATETIMEPICK_CLASS)
+  internal static let style: WindowStyle =
+      (base: DWORD(WS_BORDER | WS_TABSTOP | WS_VISIBLE), extended: 0)
+
+  /// Configuring the Date Picker Style
+
+  public private(set) var datePickerStyle: DatePickerStyle = .inline
+  public private(set) var preferredDatePickerStyle: DatePickerStyle = .automatic {
+    didSet { fatalError("not yet implemented") }
+  }
+
+  public init(frame: Rect = .default) {
+    super.init(frame: frame, class: DatePicker.class, style: DatePicker.style)
+    SetWindowSubclass(hWnd, SwiftDatePickerProc, UINT_PTR(1),
+                      unsafeBitCast(self as AnyObject, to: DWORD_PTR.self))
+  }
+}
