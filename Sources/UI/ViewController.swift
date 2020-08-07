936b06c173d917be9e1c7aa1d841bad45033a1cf
Saleem Abdulrasool <compnerd@compnerd.org>
2020-08-06 22:26:32 -0700

    UI: make `ViewController` a `ContentContainer`
    Define `ContentContainer` protocol and dependencies and make
    `ViewController` conform to it.

--- Sources/UI/ViewController.swift
+++ Sources/UI/ViewController.swift
@@ -33,6 +33,8 @@ public class ViewController {
   /// Managing the View
   public var view: View!
 
+  public var preferredContentSize: Size { fatalError("not yet implemented") }
+
   public var title: String? {
     get {
       let szLength: Int32 = GetWindowTextLengthW(view.hWnd)
@@ -57,5 +59,26 @@ public class ViewController {
   }
 
   public init() {
+  }
+}
+
+extension ViewController: ContentContainer {
+  public func willTransition(to: Size,
+                             with coodinator: ViewControllerTransitionCoordinator) {
+  }
+
+  public func willTransition(to: TraitCollection,
+                             with coordinator: ViewControllerTransitionCoordinator) {
+  }
+
+  public func size(forChildContentContainer container: ContentContainer,
+                   withParentContainerSize parentSize: Size) -> Size {
+    return .zero
+  }
+
+  public func preferredContentSizeDidChange(forChildContentContainer container: ContentContainer) {
+  }
+
+  public func systemLayoutFittingSizeDidChange(forChildContentContainer container: ContentContainer) {
   }
 }
