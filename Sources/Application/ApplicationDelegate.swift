c8d777f2f75735de987f86bab43ddd0993d0eae1
Saleem Abdulrasool <compnerd@compnerd.org>
2020-08-06 21:32:16 -0700

    Application: flesh out `ApplicationDelegate`
    Add some more entry points for application delegate callbacks that
    aren't wired up yet.  Add some notification names for
    `ApplicationDelegate`.

--- Sources/Application/ApplicationDelegate.swift
+++ Sources/Application/ApplicationDelegate.swift
@@ -27,28 +27,40 @@
  * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  **/
 
+import class Foundation.NSNotification
+
 public protocol _TriviallyConstructible {
   init()
 }
 
 public protocol ApplicationDelegate: class, _TriviallyConstructible {
-  @available(*, deprecated)
-  func applicationDidFinishLaunching(_: Application) -> Bool
+  /// Initializing the App
+  func application(_ application: Application,
+                   willFinishLaunchingWithOptions options: [Application.LaunchOptionsKey:Any]?)
+      -> Bool
 
   func application(_ application: Application,
                    didFinishLaunchingWithOptions options: [Application.LaunchOptionsKey:Any]?)
       -> Bool
 
-  // Responding to App Life-Cycle Events
+  /// Responding to App Life-Cycle Events
   func applicationDidBecomeActive(_ application: Application)
   func applicationWillResignActive(_ application: Application)
   func applicationDidEnterBackground(_ application: Application)
   func applicationWillEnterForeground(_ application: Application)
   func applicationWillTerminate(_ application: Application)
+
+  /// Responding to Environment Changes
+  func applicationProtectedDataDidBecomeAavailable(_ application: Application)
+  func applicationProtectedDataWillBecomeUnavailable(_ application: Application)
+  func applicationDidRecieveMemoryWarning(_ application: Application)
+  func applicationSignificantTimeChange(_ application: Application)
 }
 
 public extension ApplicationDelegate {
-  func applicationDidFinishLaunching(_: Application) -> Bool {
+  func application(_ application: Application,
+                   willFinishLaunchingWithOptions options: [Application.LaunchOptionsKey:Any]?)
+      -> Bool {
     return true
   }
 
@@ -59,20 +71,58 @@ public extension ApplicationDelegate {
   }
 }
 
-public extension ApplicationDelegate {
-  func applicationDidBecomeActive(_: Application) {
+extension ApplicationDelegate {
+  public func applicationDidBecomeActive(_: Application) {
   }
 
-  func applicationWillResignActive(_: Application) {
+  public func applicationWillResignActive(_: Application) {
   }
 
-  func applicationDidEnterBackground(_: Application) {
+  public func applicationDidEnterBackground(_: Application) {
   }
 
-  func applicationWillEnterForeground(_: Application) {
+  public func applicationWillEnterForeground(_: Application) {
   }
 
-  func applicationWillTerminate(_: Application) {
+  public func applicationWillTerminate(_: Application) {
+  }
+}
+
+extension ApplicationDelegate {
+  public func applicationProtectedDataDidBecomeAavailable(_ application: Application) {
+  }
+
+  public func applicationProtectedDataWillBecomeUnavailable(_ application: Application) {
+  }
+
+  public func applicationDidRecieveMemoryWarning(_ application: Application) {
+  }
+
+  public func applicationSignificantTimeChange(_ application: Application) {
+  }
+}
+
+extension ApplicationDelegate {
+  public static var didFinishLaunchingNotification: NSNotification.Name {
+    NSNotification.Name(rawValue: "UIApplicationDidFinishLaunchingNotificaton")
+  }
+}
+
+extension ApplicationDelegate {
+  public static var didBecomeActiveNotification: NSNotification.Name {
+    NSNotification.Name(rawValue: "UIApplicationDidBecomeActiveNotification")
+  }
+  public static var didEnterBackgroundNotification: NSNotification.Name {
+    NSNotification.Name(rawValue: "UIApplicationDidEnterBackgroundNotification")
+  }
+  public static var willEnterForegroundNotification: NSNotification.Name {
+    NSNotification.Name(rawValue: "UIApplicationWillEnterForegroundNotification")
+  }
+  public static var willResignActiveNotification: NSNotification.Name {
+    NSNotification.Name(rawValue: "UIApplicationWillResignActiveNotification")
+  }
+  public static var willTerminateNotification: NSNotification.Name {
+    NSNotification.Name(rawValue: "UIApplicationWillTerminateNotification")
   }
 }
 
