8ce1fafc78225a1d70bb1c6ba5087d5b11f31887
Saleem Abdulrasool <compnerd@compnerd.org>
2020-08-08 16:09:06 -0700

    Support: add additional logging levels
    This covers the full levels that `swift-log` provides allowing for use
    of additional logging levels even without `swift-log`.

--- Sources/Support/Logging.swift
+++ Sources/Support/Logging.swift
@@ -35,12 +35,32 @@ internal let log: Logger = Logger(label: "org.compnerd.swift-win32")
 import WinSDK
 
 internal struct log {
+  static func trace(_ message: String) {
+    OutputDebugStringW("TRACE: \(message)\r\n".LPCWSTR)
+  }
+
+  static func debug(_ message: String) {
+    OutputDebugStringW("DEBUG: \(message)\r\n".LPCWSTR)
+  }
+
+  static func info(_ message: String) {
+    OutputDebugStringW("INFO: \(message)\r\n".LPCWSTR)
+  }
+
+  static func notice(_ message: String) {
+    OutputDebugStringW("NOTICE: \(message)\r\n".LPCWSTR)
+  }
+
   static func error(_ message: String) {
-    OutputDebugStringW("ERROR: \(message)".LPCWSTR)
+    OutputDebugStringW("ERROR: \(message)\r\n".LPCWSTR)
   }
 
   static func warning(_ message: String) {
-    OutputDebugStringW("WARNING: \(message)".LPCWSTR)
+    OutputDebugStringW("WARNING: \(message)\r\n".LPCWSTR)
+  }
+
+  static func critical(_ message: String) {
+    OutputDebugStringW("CRITICAL: \(message)\r\n".LPCWSTR)
   }
 }
 #endif
