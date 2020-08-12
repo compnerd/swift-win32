278321df4e0f590ade2c267633dd310bac1b6c74
Saleem Abdulrasool <compnerd@compnerd.org>
2020-08-11 19:49:45 -0700

    build: add an initial Package.swift
    Using this package as a means to test the progress on
    swift-package-manager port on Windows.  This allows building Swift/Win32
    with swift-package-manager.

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let SwiftWin32 = Package(
+  name: "SwiftWin32",
+  products: [
+    .library(name: "SwiftWin32", type: .dynamic, targets: ["SwiftWin32"])
+  ],
+  targets: [
+    .target(name: "SwiftWin32",
+            path: "Sources",
+            exclude: [
+              "CMakeLists.txt",
+              "CWinRT",
+            ],
+            cSettings: [
+              .define("COBJMACROS"),
+              .define("NONAMELESSUNION"),
+            ],
+            linkerSettings: [
+              .linkedLibrary("User32"),
+              .linkedLibrary("ComCtl32"),
+            ]),
+  ]
+)
