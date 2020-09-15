8754276503e365cb47fa482cce4a3b679dd102b4
Saleem Abdulrasool <compnerd@compnerd.org>
2020-09-14 21:37:01 -0700

    UI: add typography information to font
    Define the typography information to dynamically size the font for the
    traits.

--- Sources/UI/Font.swift
+++ Sources/UI/Font.swift
@@ -40,6 +40,166 @@ extension HFONT__: HandleValue {
 
 internal typealias FontHandle = ManagedHandle<HFONT__>
 
+private typealias TypographyInfo = (weight: Font.Weight, size: Double, leading: Int)
+private let typography: [ContentSizeCategory:[Font.TextStyle:TypographyInfo]] = [
+  .extraSmall: [
+    .largeTitle:    TypographyInfo(weight: .regular, size: 31, leading: 38),
+    .title1:        TypographyInfo(weight: .regular, size: 25, leading: 31),
+    .title2:        TypographyInfo(weight: .regular, size: 19, leading: 24),
+    .title3:        TypographyInfo(weight: .regular, size: 17, leading: 22),
+    .headline:      TypographyInfo(weight: .semibold, size: 14, leading: 19),
+    .body:          TypographyInfo(weight: .regular, size: 14, leading: 19),
+    .callout:       TypographyInfo(weight: .regular, size: 13, leading: 18),
+    .subheadline:   TypographyInfo(weight: .regular, size: 12, leading: 16),
+    .footnote:      TypographyInfo(weight: .regular, size: 12, leading: 16),
+    .caption1:      TypographyInfo(weight: .regular, size: 11, leading: 13),
+    .caption2:      TypographyInfo(weight: .regular, size: 11, leading: 13),
+  ],
+  .small: [
+    .largeTitle:    TypographyInfo(weight: .regular, size: 32, leading: 39),
+    .title1:        TypographyInfo(weight: .regular, size: 26, leading: 32),
+    .title2:        TypographyInfo(weight: .regular, size: 20, leading: 25),
+    .title3:        TypographyInfo(weight: .regular, size: 18, leading: 23),
+    .headline:      TypographyInfo(weight: .semibold, size: 15, leading: 20),
+    .body:          TypographyInfo(weight: .regular, size: 15, leading: 20),
+    .callout:       TypographyInfo(weight: .regular, size: 14, leading: 19),
+    .subheadline:   TypographyInfo(weight: .regular, size: 13, leading: 18),
+    .footnote:      TypographyInfo(weight: .regular, size: 12, leading: 16),
+    .caption1:      TypographyInfo(weight: .regular, size: 11, leading: 13),
+    .caption2:      TypographyInfo(weight: .regular, size: 11, leading: 13),
+  ],
+  .medium: [
+    .largeTitle:    TypographyInfo(weight: .regular, size: 33, leading: 40),
+    .title1:        TypographyInfo(weight: .regular, size: 27, leading: 33),
+    .title2:        TypographyInfo(weight: .regular, size: 21, leading: 26),
+    .title3:        TypographyInfo(weight: .regular, size: 19, leading: 24),
+    .headline:      TypographyInfo(weight: .semibold, size: 16, leading: 21),
+    .body:          TypographyInfo(weight: .regular, size: 16, leading: 21),
+    .callout:       TypographyInfo(weight: .regular, size: 15, leading: 20),
+    .subheadline:   TypographyInfo(weight: .regular, size: 14, leading: 19),
+    .footnote:      TypographyInfo(weight: .regular, size: 12, leading: 16),
+    .caption1:      TypographyInfo(weight: .regular, size: 11, leading: 13),
+    .caption2:      TypographyInfo(weight: .regular, size: 11, leading: 13),
+  ],
+  .large: [
+    .largeTitle:    TypographyInfo(weight: .regular, size: 34, leading: 41),
+    .title1:        TypographyInfo(weight: .regular, size: 28, leading: 34),
+    .title2:        TypographyInfo(weight: .regular, size: 22, leading: 28),
+    .title3:        TypographyInfo(weight: .regular, size: 20, leading: 25),
+    .headline:      TypographyInfo(weight: .semibold, size: 17, leading: 22),
+    .body:          TypographyInfo(weight: .regular, size: 17, leading: 22),
+    .callout:       TypographyInfo(weight: .regular, size: 16, leading: 21),
+    .subheadline:   TypographyInfo(weight: .regular, size: 15, leading: 20),
+    .footnote:      TypographyInfo(weight: .regular, size: 13, leading: 18),
+    .caption1:      TypographyInfo(weight: .regular, size: 12, leading: 16),
+    .caption2:      TypographyInfo(weight: .regular, size: 11, leading: 13),
+  ],
+  .extraLarge: [
+    .largeTitle:    TypographyInfo(weight: .regular, size: 36, leading: 43),
+    .title1:        TypographyInfo(weight: .regular, size: 30, leading: 37),
+    .title2:        TypographyInfo(weight: .regular, size: 24, leading: 30),
+    .title3:        TypographyInfo(weight: .regular, size: 22, leading: 28),
+    .headline:      TypographyInfo(weight: .semibold, size: 19, leading: 24),
+    .body:          TypographyInfo(weight: .regular, size: 19, leading: 24),
+    .callout:       TypographyInfo(weight: .regular, size: 18, leading: 23),
+    .subheadline:   TypographyInfo(weight: .regular, size: 17, leading: 22),
+    .footnote:      TypographyInfo(weight: .regular, size: 15, leading: 20),
+    .caption1:      TypographyInfo(weight: .regular, size: 14, leading: 19),
+    .caption2:      TypographyInfo(weight: .regular, size: 13, leading: 18),
+  ],
+  .extraExtraLarge: [
+    .largeTitle:    TypographyInfo(weight: .regular, size: 38, leading: 46),
+    .title1:        TypographyInfo(weight: .regular, size: 32, leading: 39),
+    .title2:        TypographyInfo(weight: .regular, size: 26, leading: 32),
+    .title3:        TypographyInfo(weight: .regular, size: 24, leading: 30),
+    .headline:      TypographyInfo(weight: .semibold, size: 21, leading: 26),
+    .body:          TypographyInfo(weight: .regular, size: 21, leading: 26),
+    .callout:       TypographyInfo(weight: .regular, size: 20, leading: 25),
+    .subheadline:   TypographyInfo(weight: .regular, size: 19, leading: 24),
+    .footnote:      TypographyInfo(weight: .regular, size: 17, leading: 22),
+    .caption1:      TypographyInfo(weight: .regular, size: 16, leading: 21),
+    .caption2:      TypographyInfo(weight: .regular, size: 15, leading: 20),
+  ],
+  .extraExtraExtraLarge: [
+    .largeTitle:    TypographyInfo(weight: .regular, size: 40, leading: 48),
+    .title1:        TypographyInfo(weight: .regular, size: 34, leading: 41),
+    .title2:        TypographyInfo(weight: .regular, size: 28, leading: 34),
+    .title3:        TypographyInfo(weight: .regular, size: 26, leading: 32),
+    .headline:      TypographyInfo(weight: .semibold, size: 23, leading: 29),
+    .body:          TypographyInfo(weight: .regular, size: 23, leading: 29),
+    .callout:       TypographyInfo(weight: .regular, size: 22, leading: 28),
+    .subheadline:   TypographyInfo(weight: .regular, size: 21, leading: 28),
+    .footnote:      TypographyInfo(weight: .regular, size: 19, leading: 24),
+    .caption1:      TypographyInfo(weight: .regular, size: 18, leading: 23),
+    .caption2:      TypographyInfo(weight: .regular, size: 17, leading: 22),
+  ],
+  .accessibilityMedium: [
+    .largeTitle:    TypographyInfo(weight: .regular, size: 44, leading: 52),
+    .title1:        TypographyInfo(weight: .regular, size: 38, leading: 46),
+    .title2:        TypographyInfo(weight: .regular, size: 34, leading: 41),
+    .title3:        TypographyInfo(weight: .regular, size: 31, leading: 38),
+    .headline:      TypographyInfo(weight: .semibold, size: 28, leading: 34),
+    .body:          TypographyInfo(weight: .regular, size: 28, leading: 34),
+    .callout:       TypographyInfo(weight: .regular, size: 26, leading: 32),
+    .subheadline:   TypographyInfo(weight: .regular, size: 25, leading: 31),
+    .footnote:      TypographyInfo(weight: .regular, size: 23, leading: 29),
+    .caption1:      TypographyInfo(weight: .regular, size: 22, leading: 28),
+    .caption2:      TypographyInfo(weight: .regular, size: 20, leading: 25),
+  ],
+  .accessibilityLarge: [
+    .largeTitle:    TypographyInfo(weight: .regular, size: 48, leading: 57),
+    .title1:        TypographyInfo(weight: .regular, size: 43, leading: 51),
+    .title2:        TypographyInfo(weight: .regular, size: 39, leading: 47),
+    .title3:        TypographyInfo(weight: .regular, size: 37, leading: 44),
+    .headline:      TypographyInfo(weight: .semibold, size: 33, leading: 40),
+    .body:          TypographyInfo(weight: .regular, size: 33, leading: 40),
+    .callout:       TypographyInfo(weight: .regular, size: 32, leading: 39),
+    .subheadline:   TypographyInfo(weight: .regular, size: 30, leading: 37),
+    .footnote:      TypographyInfo(weight: .regular, size: 27, leading: 33),
+    .caption1:      TypographyInfo(weight: .regular, size: 26, leading: 32),
+    .caption2:      TypographyInfo(weight: .regular, size: 24, leading: 30),
+  ],
+  .accessibilityExtraLarge: [
+    .largeTitle:    TypographyInfo(weight: .regular, size: 52, leading: 61),
+    .title1:        TypographyInfo(weight: .regular, size: 48, leading: 57),
+    .title2:        TypographyInfo(weight: .regular, size: 44, leading: 52),
+    .title3:        TypographyInfo(weight: .regular, size: 43, leading: 51),
+    .headline:      TypographyInfo(weight: .semibold, size: 40, leading: 48),
+    .body:          TypographyInfo(weight: .regular, size: 40, leading: 48),
+    .callout:       TypographyInfo(weight: .regular, size: 38, leading: 46),
+    .subheadline:   TypographyInfo(weight: .regular, size: 36, leading: 43),
+    .footnote:      TypographyInfo(weight: .regular, size: 33, leading: 40),
+    .caption1:      TypographyInfo(weight: .regular, size: 32, leading: 39),
+    .caption2:      TypographyInfo(weight: .regular, size: 29, leading: 35),
+  ],
+  .accessibilityExtraExtraLarge: [
+    .largeTitle:    TypographyInfo(weight: .regular, size: 56, leading: 66),
+    .title1:        TypographyInfo(weight: .regular, size: 53, leading: 62),
+    .title2:        TypographyInfo(weight: .regular, size: 50, leading: 59),
+    .title3:        TypographyInfo(weight: .regular, size: 49, leading: 58),
+    .headline:      TypographyInfo(weight: .semibold, size: 47, leading: 56),
+    .body:          TypographyInfo(weight: .regular, size: 47, leading: 56),
+    .callout:       TypographyInfo(weight: .regular, size: 44, leading: 52),
+    .subheadline:   TypographyInfo(weight: .regular, size: 42, leading: 50),
+    .footnote:      TypographyInfo(weight: .regular, size: 38, leading: 46),
+    .caption1:      TypographyInfo(weight: .regular, size: 37, leading: 44),
+    .caption2:      TypographyInfo(weight: .regular, size: 34, leading: 41),
+  ],
+  .accessibilityExtraExtraExtraLarge: [
+    .largeTitle:    TypographyInfo(weight: .regular, size: 60, leading: 70),
+    .title1:        TypographyInfo(weight: .regular, size: 58, leading: 68),
+    .title2:        TypographyInfo(weight: .regular, size: 56, leading: 66),
+    .title3:        TypographyInfo(weight: .regular, size: 55, leading: 65),
+    .headline:      TypographyInfo(weight: .semibold, size: 53, leading: 62),
+    .body:          TypographyInfo(weight: .regular, size: 53, leading: 62),
+    .callout:       TypographyInfo(weight: .regular, size: 51, leading: 62),
+    .subheadline:   TypographyInfo(weight: .regular, size: 49, leading: 58),
+    .footnote:      TypographyInfo(weight: .regular, size: 44, leading: 52),
+    .caption1:      TypographyInfo(weight: .regular, size: 43, leading: 51),
+    .caption2:      TypographyInfo(weight: .regular, size: 40, leading: 47),
+  ],
+]
+
 private func PointToLogical(_ points: Double) -> Int32 {
   return -MulDiv(Int32(points), GetDeviceCaps(GetDC(nil), LOGPIXELSY), 72)
 }
@@ -58,35 +218,17 @@ public class Font {
   }
 
   public static func preferredFont(forTextStyle style: Font.TextStyle,
-                                   compatibleWith traitCollection: TraitCollection?)
+                                   compatibleWith traits: TraitCollection?)
       -> Font {
-    switch style {
-    case .body:
-      return systemFont(ofSize: 17)
-    case .callout:
-      return systemFont(ofSize: 16)
-    case .caption1:
-      return systemFont(ofSize: 12)
-    case .caption2:
-      return systemFont(ofSize: 11)
-    case .footnote:
-      return systemFont(ofSize: 13)
-    case .headline:
-      return systemFont(ofSize: 17, weight: .semibold)
-    case .subheadline:
-      return systemFont(ofSize: 15)
-    case .largeTitle:
-      return systemFont(ofSize: 34)
-    case .title1:
-      return systemFont(ofSize: 28)
-    case .title2:
-      return systemFont(ofSize: 22)
-    case .title3:
-      return systemFont(ofSize: 20)
-    default:
-      log.warning("unknown text style: \(style) - assuming 12pt system font")
+    var size: ContentSizeCategory = traits?.preferredContentSizeCategory ?? .large
+    if size == .unspecified { size = .large }
+
+    guard let font = typography[size]?[style] else {
+      log.error("\(#function) missing typography for \(style)@\(size)")
       return systemFont(ofSize: 12)
     }
+
+    return systemFont(ofSize: font.size, weight: font.weight)
   }
 
   public init?(name: String, size: Double) {
