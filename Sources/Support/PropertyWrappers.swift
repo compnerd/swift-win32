85ccabf0aa928aae867d2ea0d19d2e5406e865e2
Saleem Abdulrasool <compnerd@compnerd.org>
2020-09-19 16:43:57 -0700

    Remove <5.3 compatibility
    Remove the compatibility paths for Swift 5.2 as Swift 5.3 is now
    required.

--- Sources/Support/PropertyWrappers.swift
+++ Sources/Support/PropertyWrappers.swift
@@ -22,19 +22,10 @@ public struct _Win32WindowText {
       let szLength: Int32 = GetWindowTextLengthW(view.hWnd)
       guard szLength > 0 else { return nil }
 
-#if swift(<5.3)
-      let buffer: UnsafeMutablePointer<WCHAR> =
-        UnsafeMutablePointer<WCHAR>.allocate(capacity: Int(szLength) + 1)
-      defer { buffer.deallocate() }
-
-      GetWindowTextW(view.hWnd, buffer, szLength + 1)
-      return String(decodingCString: buffer, as: UTF16.self)
-#else
       let buffer: [WCHAR] = Array<WCHAR>(unsafeUninitializedCapacity: Int(szLength) + 1) {
         $1 = Int(GetWindowTextW(view.hWnd, $0.baseAddress!, CInt($0.count)))
       }
       return String(decodingCString: buffer, as: UTF16.self)
-#endif
     }
     set(value) {
       SetWindowTextW(view.hWnd, value?.LPCWSTR)
--- Sources/UI/ViewController.swift
+++ Sources/UI/ViewController.swift
@@ -16,20 +16,10 @@ public class ViewController {
   public var title: String? {
     get {
       let szLength: Int32 = GetWindowTextLengthW(view.hWnd)
-
-#if swift(<5.3)
-      let buffer: UnsafeMutablePointer<WCHAR> =
-        UnsafeMutablePointer<WCHAR>.allocate(capacity: Int(szLength) + 1)
-      defer { buffer.deallocate() }
-
-      GetWindowTextW(view.hWnd, buffer, szLength + 1)
-      return String(decodingCString: buffer, as: UTF16.self)
-#else
       let buffer: [WCHAR] = Array<WCHAR>(unsafeUninitializedCapacity: Int(szLength) + 1) {
         $1 = Int(GetWindowTextW(view.hWnd, $0.baseAddress!, CInt($0.count)))
       }
       return String(decodingCString: buffer, as: UTF16.self)
-#endif
     }
     set(value) {
       SetWindowTextW(view.hWnd, value?.LPCWSTR)
