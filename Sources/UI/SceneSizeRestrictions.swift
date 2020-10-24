5485ae0bd8d5afb7670a3ec95c3f37fd937c0ac4
Saleem Abdulrasool <compnerd@compnerd.org>
2020-10-23 20:00:52 -0700

    UI: adjust the default window size
    Default the window size restriction to 768x768.

--- Sources/UI/SceneSizeRestrictions.swift
+++ Sources/UI/SceneSizeRestrictions.swift
@@ -9,8 +9,8 @@ public class SceneSizeRestrictions {
   /// Setting the Size Restrictions
 
   /// The minimum width and height supported by your application's windows.
-  public var minimumSize: Size = .zero
+  public var minimumSize: Size = Size(width: 768, height: 768)
 
   /// The maximum width and height supported by your application's windows.
-  public var maximumSize: Size = .zero
+  public var maximumSize: Size = Size(width: 768, height: 768)
 }
