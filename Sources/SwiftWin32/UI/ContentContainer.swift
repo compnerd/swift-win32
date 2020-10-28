851a47c5ea032f0cf3977bdd0dfdb529f0cded42
Saleem Abdulrasool <compnerd@compnerd.org>
2020-10-27 22:26:18 -0700

    UI: add some documentation for `ContentContainer`
    Add some doc comments to the interface (NFC).

--- Sources/SwiftWin32/UI/ContentContainer.swift
+++ Sources/SwiftWin32/UI/ContentContainer.swift
@@ -5,18 +5,33 @@
  * SPDX-License-Identifier: BSD-3-Clause
  **/
 
+/// A set of methods for adapting the contents of your view controllers to size
+/// and trait changes.
 public protocol ContentContainer {
-  /// Responding to Environment Changes
+  // MARK - Responding to Environment Changes
+
+  /// Notifies the container that the size of its view is about to change.
   func willTransition(to: Size,
                       with coodinator: ViewControllerTransitionCoordinator)
+
+  /// Notifies the container that its trait collection changed.
   func willTransition(to: TraitCollection,
                       with coordinator: ViewControllerTransitionCoordinator)
 
-  /// Responding to Changes in Child View Controllers
+  // MARK - Responding to Changes in Child View Controllers
+
+  /// The preferred size for the container's content.
   var preferredContentSize: Size { get }
 
+  /// Returns the size of the specified child view controller's content.
   func size(forChildContentContainer container: ContentContainer,
             withParentContainerSize parentSize: Size) -> Size
+
+  /// Notifies an interested controller that the preferred content size of one of
+  /// its children changed.
   func preferredContentSizeDidChange(forChildContentContainer container: ContentContainer)
+
+  /// Notifies the container that a child view controller was resized using auto
+  /// layout.
   func systemLayoutFittingSizeDidChange(forChildContentContainer container: ContentContainer)
 }
