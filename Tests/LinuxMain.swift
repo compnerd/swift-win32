ffb4eddc72b24bd5edbe4e6289938e75cfadfdd6
Saleem Abdulrasool <compnerd@compnerd.org>
2020-11-03 17:39:04 -0800

    Tests: add an initial test harness setup
    Add some test harness for enabling testing `LayoutConstraint`.  The
    layout constraint paths are not rendering related and can be tested more
    readily.

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,15 @@
+/**
+ * Copyright © 2020 Saleem Abdulrasool <compnerd@compnerd.org>.
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ **/
+
+import XCTest
+
+@testable
+import AutoLayoutTests
+
+XCTMain([
+  testCase(AutoLayoutTests.allTests),
+])
