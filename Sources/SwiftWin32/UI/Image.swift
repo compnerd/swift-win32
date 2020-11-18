7cd83a60477c54a5a4e84440267e471432cbd839
Saleem Abdulrasool <compnerd@compnerd.org>
2020-11-17 21:33:08 -0800

    UI: add `Image` type
    This allows us to load an image from a file and convert it to a form
    that we can render to a `HBITMAP` enabling us to use this to render it
    to a GDI context (Window).

--- Sources/SwiftWin32/UI/Image.swift
+++ Sources/SwiftWin32/UI/Image.swift
@@ -5,5 +5,46 @@
  * SPDX-License-Identifier: BSD-3-Clause
  **/
 
+import WinSDK
+import SwiftCOM
+
+private let WICImagingFactory: SwiftCOM.IWICImagingFactory? =
+    try? IWICImagingFactory.CreateInstance(class: CLSID_WICImagingFactory)
+// FIXME(compnerd) we are currently leaking the WICImagingFactory
+// defer { WICImagingFactory?.Release() }
+
 public class Image {
+  private var WICBitmapDecoder: SwiftCOM.IWICBitmapDecoder?
+  private var WICBitmapFrame: SwiftCOM.IWICBitmapFrameDecode?
+  private var WICFormatConverter: SwiftCOM.IWICFormatConverter?
+
+  public init?(contentsOfFile path: String) {
+    guard let WICImagingFactory = WICImagingFactory else { return nil }
+
+    do {
+      self.WICBitmapDecoder =
+          try WICImagingFactory.CreateDecoderFromFilename(path, nil, GENERIC_READ,
+                                                          WICDecodeMetadataCacheOnDemand)
+
+      // TODO(compnerd) figure out how to handle multi-frame images
+      let frames = try self.WICBitmapDecoder?.GetFrameCount()
+
+      self.WICBitmapFrame = try self.WICBitmapDecoder?.GetFrame(frames! - 1)
+
+      self.WICFormatConverter = try WICImagingFactory.CreateFormatConverter()
+      try self.WICFormatConverter?.Initialize(WICBitmapFrame!,
+                                              GUID_WICPixelFormat32bppBGRA,
+                                              WICBitmapDitherTypeNone, nil, 0.0,
+                                              WICBitmapPaletteTypeCustom)
+    } catch {
+      log.error("\(#function): \(error)")
+      return nil
+    }
+  }
+
+  deinit {
+    _ = try? self.WICFormatConverter?.Release()
+    _ = try? self.WICBitmapFrame?.Release()
+    _ = try? self.WICBitmapDecoder?.Release()
+  }
 }
