98966a866b5406e058783074b805294863a3db0c
Saleem Abdulrasool <compnerd@compnerd.org>
2020-12-05 18:52:06 -0800

    UI: interfaces for supporting TableViewDelegate

---
+++ Sources/SwiftWin32/UI/SpringLoadedInteractionContext.swift
@@ -0,0 +1,34 @@
+/**
+ * Copyright © 2020 Saleem Abdulrasool <compnerd@compnerd.org>
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ **/
+
+/// The spring-loaded interaction states that determine the style of the
+/// interaction view.
+public enum SpringLoadedInteractionEffectState: Int {
+/// An interaction state that indicates that the view was spring loaded.
+case activated
+/// An interaction state that indicates that spring loading is about to start.
+case activating
+/// An interaction state that indicates that spring loading is not engaged.
+case inactive
+/// An interaction state that indicates that spring loading is available.
+case possible
+}
+
+/// The interface an object implements to provide information about a
+/// spring-loaded interaction.
+public protocol SpringLoadedInteractionContext {
+  /// The current view style for the string-loaded interaction.
+  var state: SpringLoadedInteractionEffectState { get }
+
+  /// The specific subview, or associated model object, of the target view to
+  /// use for the spring-loaded interaction.
+  var targetItem: Any? { get set }
+
+  /// The view to which the current spring-loaded interaction view style is
+  /// applied.
+  var targetView: View? { get set }
+}
---
+++ Sources/SwiftWin32/UI/SwipeActionsConfiguration.swift
@@ -0,0 +1,27 @@
+/**
+ * Copyright © 2020 Saleem Abdulrasool <compnerd@compnerd.org>
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ **/
+
+/// The set of actions to perform when swiping on rows of a table.
+public class SwipeActionsConfiguration {
+  // MARK - Initializing the Swipe Actions
+
+  /// Creates a swipe action configuration object with the specified set of
+  /// actions.
+  public init(actions: [ContextualAction]) {
+  // convenience
+    self.actions = actions
+  }
+
+  // MARK - Getting the Swipe Action Information
+
+  /// The swipe actions.
+  public private(set) var actions: [ContextualAction]
+
+  /// A boolean value indicating whether a full swipe automatically performs the
+  /// first action.
+  public var performsFirstActionWithFullSwipe: Bool = true
+}
