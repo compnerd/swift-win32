119de8723443372a6fddf2425e1caa6dc1d25ecb
Saleem Abdulrasool <compnerd@compnerd.org>
2020-12-10 20:36:28 -0800

    AutoLayout: improve the factory methods for `LayoutGuide`
    Adjust the layout guide constructors to make more sense.

--- Sources/SwiftWin32/AutoLayout/LayoutAnchor.swift
+++ Sources/SwiftWin32/AutoLayout/LayoutAnchor.swift
@@ -7,10 +7,10 @@
 
 /// A factory class for creating layout constraint objects using a fluent API.
 public class LayoutAnchor<AnchorType: AnyObject> {
-  internal weak var item: AnyObject?
+  internal unowned var item: AnyObject
   internal var attribute: LayoutConstraint.Attribute
 
-  internal init(item: AnyObject?, attribute: LayoutConstraint.Attribute) {
+  internal init(item: AnyObject, attribute: LayoutConstraint.Attribute) {
     self.item = item
     self.attribute = attribute
   }
@@ -21,7 +21,9 @@ public class LayoutAnchor<AnchorType: AnyObject> {
   /// another.
   public func constraint(equalTo anchor: LayoutAnchor<AnchorType>)
       -> LayoutConstraint {
-    return LayoutConstraint(anchor: self, relatedBy: .equal, toAnchor: anchor,
+    return LayoutConstraint(item: self.item, attribute: self.attribute,
+                            relatedBy: .equal,
+                            toItem: anchor.item, attribute: anchor.attribute,
                             multiplier: 1.0, constant: 0.0)
   }
 
@@ -29,7 +31,9 @@ public class LayoutAnchor<AnchorType: AnyObject> {
   /// item's attribute plus a constant offset.
   public func constraint(equalTo anchor: LayoutAnchor<AnchorType>,
                          constant offset: Double) -> LayoutConstraint {
-    return LayoutConstraint(anchor: self, relatedBy: .equal, toAnchor: anchor,
+    return LayoutConstraint(item: self.item, attribute: self.attribute,
+                            relatedBy: .equal,
+                            toItem: anchor.item, attribute: anchor.attribute,
                             multiplier: 1.0, constant: offset)
   }
 
@@ -37,37 +41,39 @@ public class LayoutAnchor<AnchorType: AnyObject> {
   /// equal to another.
   public func constraint(greaterThanOrEqualTo anchor: LayoutAnchor<AnchorType>)
       -> LayoutConstraint {
-    return LayoutConstraint(anchor: self, relatedBy: .greaterThanOrEqual,
-                            toAnchor: anchor, multiplier: 1.0, constant: 0.0)
+    return LayoutConstraint(item: self.item, attribute: self.attribute,
+                            relatedBy: .greaterThanOrEqual,
+                            toItem: anchor.item, attribute: anchor.attribute,
+                            multiplier: 1.0, constant: 0.0)
   }
 
   /// Returns a constraint that defines one item's attribute as greater than or
   /// equal to another item's attribute plus a constant offset.
   public func constraint(greaterThanOrEqualTo anchor: LayoutAnchor<AnchorType>,
                          constant offset: Double) -> LayoutConstraint {
-    return LayoutConstraint(anchor: self, relatedBy: .greaterThanOrEqual,
-                            toAnchor: anchor, multiplier: 1.0, constant: offset)
+    return LayoutConstraint(item: self.item, attribute: self.attribute,
+                            relatedBy: .greaterThanOrEqual,
+                            toItem: anchor.item, attribute: anchor.attribute,
+                            multiplier: 1.0, constant: offset)
   }
 
   /// Returns a constraint that defines one item's attribute as less than or
   /// equal to another.
   public func constraint(lessThanOrEqualTo anchor: LayoutAnchor<AnchorType>)
       -> LayoutConstraint {
-    return LayoutConstraint(anchor: self, relatedBy: .lessThanOrEqual,
-                            toAnchor: anchor, multiplier: 1.0, constant: 0.0)
+    return LayoutConstraint(item: self.item, attribute: self.attribute,
+                            relatedBy: .lessThanOrEqual,
+                            toItem: anchor.item, attribute: anchor.attribute,
+                            multiplier: 1.0, constant: 0.0)
   }
 
   /// Returns a constraint that defines one item's attribute as less than or
   /// equal to another item's attribute plus a constant offset.
   public func constraint(lessThanOrEqualTo anchor: LayoutAnchor<AnchorType>,
                          constannt offset: Double) -> LayoutConstraint {
-    return LayoutConstraint(anchor: self, relatedBy: .lessThanOrEqual,
-                            toAnchor: anchor, multiplier: 1.0, constant: offset)
-  }
-}
-
-extension LayoutAnchor {
-  internal var generic: LayoutAnchor<AnyObject> {
-    LayoutAnchor<AnyObject>(item: self.item, attribute: self.attribute)
+    return LayoutConstraint(item: self.item, attribute: self.attribute,
+                            relatedBy: .lessThanOrEqual,
+                            toItem: anchor.item, attribute: self.attribute,
+                            multiplier: 1.0, constant: offset)
   }
 }
