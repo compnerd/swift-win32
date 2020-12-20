3a2a1b1c4957f6eb5b089c1fb711a84582811367
Saleem Abdulrasool <compnerd@compnerd.org>
2020-12-20 10:50:07 -0800

    UI: add focus based navigation types
    This adds some of the basic structures for the focus based navigation
    infrastructure.  This is meant to support navigation with alternate
    input systems (e.g. gamepads).

---
+++ Sources/SwiftWin32/UI/Focus-Based Navigation/FocusItem.swift
@@ -0,0 +1,35 @@
+/**
+ * Copyright © 2020 Saleem Abdulrasool <compnerd@compnerd.org>
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ **/
+
+/// An object that can become focused.
+public protocol FocusItem: FocusEnvironment {
+  // MARK - Determining Focusability
+
+  /// A boolean value that indicates whether the item can become focused.
+  var canBecomeFocused: Bool { get }
+
+  /// A boolean value indicating whether the item is currently focused.
+  var isFocused: Bool { get }
+
+  // MARK - Retrieving the Item Frame
+
+  /// The geometric frame of the item.
+  var frame: Rect { get }
+
+  // MARK - Providing Movement Hints
+
+  func didHintFocusMovement(_ hint: FocusMovementHint)
+}
+
+extension FocusItem {
+  public var isFocused: Bool { false }
+}
+
+extension FocusItem {
+  public func didHintFocusMovement(_ hint: FocusMovementHint) {
+  }
+}
---
+++ Sources/SwiftWin32/UI/Focus-Based Navigation/FocusItemContainer.swift
@@ -0,0 +1,20 @@
+/**
+ * Copyright © 2020 Saleem Abdulrasool <compnerd@compnerd.org>
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ **/
+
+/// The container responsible for providing geometric context to focus items
+/// within a given focus environment.
+public protocol FocusItemContainer {
+  // MARK - Retrieving Focus Items
+
+  /// Retrieves all of the focus items within this container that intersect with
+  /// the provided rectangle.
+  func focusItems(in rect: Rect) -> [FocusItem]
+
+  /// The coordinate space of the focus items contained in the focus item
+  /// container.
+  var coordinateSpace: CoordinateSpace { get }
+}
