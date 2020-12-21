d63ae705723261d51889a6be48895a1dcd06c808
Saleem Abdulrasool <compnerd@compnerd.org>
2020-12-20 17:20:26 -0800

    UI: add docs to `AlertAction`, `AlertController`
    Add additional documentation for the members of these types.

--- Sources/SwiftWin32/Windows and Screens/AlertAction.swift
+++ Sources/SwiftWin32/Windows and Screens/AlertAction.swift
@@ -2,17 +2,25 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 extension AlertAction {
+  /// Styles to apply to action buttons in an alert.
   public enum Style: Int {
-  case `default`
-  case cancel
-  case destructive
+    /// Apply the default style to the action’s button.
+    case `default`
+
+    /// Apply a style that indicates the action cancels the operation and leaves
+    /// things unchanged.
+    case cancel
+
+    /// Apply a style that indicates the action might change or delete data.
+    case destructive
   }
 }
 
+/// An action that can be taken when the user taps a button in an alert.
 public class AlertAction {
   private let handler: ((AlertAction) -> Void)?
 
-  /// Creating an Alert Action
+  // MARK - Creating an Alert Action
 
   /// Create and return an action with the specified title and behavior.
   public init(title: String?, style: AlertAction.Style,
@@ -23,7 +31,7 @@ public class AlertAction {
     self.handler = handler
   }
 
-  /// Getting the Action's Attributes
+  // MARK - Getting the Action's Attributes
 
   /// The title of the action’s button.
   public let title: String?
