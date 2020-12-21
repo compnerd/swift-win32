8d0fc15b2043b0aa0052ba140d9763de94c139fd
Saleem Abdulrasool <compnerd@compnerd.org>
2020-12-20 17:20:26 -0800

    UI: add docs to `AlertAction`, `AlertController`
    Add additional documentation for the members of these types.

--- Sources/SwiftWin32/Windows and Screens/AlertController.swift
+++ Sources/SwiftWin32/Windows and Screens/AlertController.swift
@@ -4,17 +4,29 @@
 extension AlertController {
   /// Constants indicating the type of alert to display.
   public enum Style: Int {
+  /// An action sheet displayed in the context of the view controller that
+  /// presented it.
   case actionSheet
+
+  /// An alert displayed modally for the app.
   case alert
   }
 }
 
+/// An object that displays an alert message to the user.
 public class AlertController: ViewController {
-  /// Configuring the Alert
+  // MARK - Configuring the Alert
+
+  /// Descriptive text that provides more details about the reason for the
+  /// alert.
   public var message: String?
+
+  /// The style of the alert controller.
   public let preferredStyle: AlertController.Style
 
-  /// Creating an Alert Controller
+  // MARK - Creating an Alert Controller
+
+  /// Creates and returns a view controller for displaying an alert to the user.
   public init(title: String?, message: String?,
               preferredStyle: AlertController.Style) {
     self.message = message
@@ -22,7 +34,11 @@ public class AlertController: ViewController {
     super.init()
   }
 
-  /// Configuring the User Actions
+  // MARK - Configuring the User Actions
+
+  /// Attaches an action object to the alert or action sheet.
+  public func addAction(_ action: AlertAction) {
+  }
 
   /// The actions that the user can take in response to the alert or action
   /// sheet.
@@ -31,16 +47,12 @@ public class AlertController: ViewController {
   /// The preferred action for the user to take from an alert.
   public var preferredAction: AlertAction?
 
-  /// Attaches an action object to the alert or action sheet.
-  public func addAction(_ action: AlertAction) {
-  }
-
-  /// Configuring Text Fields
-
-  /// The array of text fields displayed by the alert.
-  public private(set) var textFields: [TextField]?
+  // MARK - Configuring Text Fields
 
   /// Adds a text field to an alert.
   public func addTextField(configurationHandler: ((TextField) -> Void)? = nil) {
   }
+
+  /// The array of text fields displayed by the alert.
+  public private(set) var textFields: [TextField]?
 }
