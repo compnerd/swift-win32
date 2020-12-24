5c87beeef58c9c57e12413977f1ac5836a5891a5
Saleem Abdulrasool <compnerd@compnerd.org>
2020-12-24 14:38:08 -0800

    Appearance Customization: add definition of `ConfigurationColorTransformer`
    Add definition for `ConfigurationColorTransformer`.

---
+++ Sources/SwiftWin32/Appearance Customization/Configurations/ConfigurationColorTransformer.swift
@@ -0,0 +1,42 @@
+/**
+ * Copyright © 2020 Saleem Abdulrasool <compnerd@compnerd.org>
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ **/
+
+/// A transformer that generates a modified output color from an input color.
+public struct ConfigurationColorTransformer {
+  // MARK - Creating a Color Transformer
+
+  /// Creates a color transformer with the specified closure.
+  public init(_ transform: @escaping (Color) -> Color) {
+    self.transform = transform
+  }
+
+  /// Creates a color transformer that generates a grayscale version of the
+  /// color.
+  public static var grayscale: ConfigurationColorTransformer {
+    fatalError("\(#function) not yet implemented")
+  }
+
+  /// A color transformer that returns the preferred system accent color.
+  public static var preferredTint: ConfigurationColorTransformer {
+    fatalError("\(#function) not yet implemented")
+  }
+
+  /// A color transformer that returns the color with a monochrome tint.
+  public static var monochromeTint: ConfigurationColorTransformer {
+    fatalError("\(#function) not yet implemented")
+  }
+
+  // MARK - Calling the Color Transformer
+
+  /// The transform closure of the color transformer.
+  public private(set) var transform: (Color) -> Color
+
+  /// Calls the transform closure of the color transformer.
+  public func callAsFunction(_ input: Color) -> Color {
+    return self.transform(input)
+  }
+}
