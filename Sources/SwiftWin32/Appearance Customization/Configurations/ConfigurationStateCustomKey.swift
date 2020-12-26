c779cd1517cf947ca662fdd05fac9debd7dcc490
Saleem Abdulrasool <compnerd@compnerd.org>
2020-12-25 16:18:00 -0800

    Appearance Customization: split out `ConfigurationStateCustomKey`
    Split out the definition to a standalone file.

---
+++ Sources/SwiftWin32/Appearance Customization/Configurations/ConfigurationStateCustomKey.swift
@@ -0,0 +1,23 @@
+/**
+ * Copyright © 2020 Saleem Abdulrasool <compnerd@compnerd.org>
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ **/
+
+/// A key that defines a custom state for a view.
+public struct ConfigurationStateCustomKey: Equatable, Hashable, RawRepresentable {
+  public typealias RawValue = String
+
+  public let rawValue: RawValue
+
+  public init(rawValue: RawValue) {
+    self.rawValue = rawValue
+  }
+}
+
+extension ConfigurationStateCustomKey {
+  public init(_ rawValue: String) {
+    self.rawValue = rawValue
+  }
+}
