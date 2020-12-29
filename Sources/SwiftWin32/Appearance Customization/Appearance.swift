575a9dc702b14c98a8e18abfec62d64b7dd18faf
Saleem Abdulrasool <compnerd@compnerd.org>
2020-12-28 21:11:42 -0800

    Appearance Customization: add `Appearance` protocol
    Add the `Appearance` protocol definition.

---
+++ Sources/SwiftWin32/Appearance Customization/Appearance.swift
@@ -0,0 +1,30 @@
+/**
+ * Copyright © 2020 Saleem Abdulrasool <compnerd@compnerd.org>
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ **/
+
+/// A collection of methods that gives you access to the appearance proxy for a
+/// class.
+public protocol Appearance {
+  // MARK - Appearance Methods
+
+  /// Returns the appearance proxy for the receiver.
+  static func appearance() -> Self
+
+  /// Returns the appearance proxy for the receiver that has the passed trait
+  /// collection.
+  static func appearance(for trait: TraitCollection) -> Self
+
+  /// Returns the appearance proxy for the object when it is contained in the
+  /// hierarchy the specified classes describe.
+  static func appearance(whenContainedInInstancesOf containerTypes: [AppearanceContainer.Type])
+      -> Self
+
+  /// Returns the appearance proxy for the object when it is contained in the
+  /// hierarchy the specified classes describe and has the specified trait
+  /// collection.
+  static func appearance(for trait: TraitCollection,
+                         whenContainedInInstancesOf containerTypes: [AppearanceContainer.Type]) -> Self
+}
