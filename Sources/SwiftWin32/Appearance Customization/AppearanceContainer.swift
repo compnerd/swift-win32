076ebb37e48f2d3122406edb79df10143a9a4a5b
Saleem Abdulrasool <compnerd@compnerd.org>
2020-12-28 21:11:42 -0800

    Appearance Customization: add `Appearance` protocol
    Add the `Appearance` protocol definition.

---
+++ Sources/SwiftWin32/Appearance Customization/AppearanceContainer.swift
@@ -0,0 +1,11 @@
+/**
+ * Copyright © 2020 Saleem Abdulrasool <compnerd@compnerd.org>
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ **/
+
+/// A protocol that a class must adopt to allow appearance customization using
+/// the `Appearance` API.
+public protocol AppearanceContainer {
+}
