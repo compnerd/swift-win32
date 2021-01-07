63b1bbb90697d1ad1609aa174b55f89181312ac4
Egor Zhdan <egor.zhdan@gmail.com>
2021-01-07 18:11:09 +0300

    UI: support icons in menu items

--- Sources/SwiftWin32/Menus and Shortcuts/MenuElement.swift
+++ Sources/SwiftWin32/Menus and Shortcuts/MenuElement.swift
@@ -73,15 +73,25 @@ public class MenuElement {
 internal class Win32MenuElement {
   private var title: [WCHAR]
   private let submenu: Win32Menu?
+  private let image: BitmapHandle?
 
   internal var info: MENUITEMINFOW
 
-  private init(title: String, submenu: Win32Menu?, fType: Int32) {
+  private init(title: String, image: Image?, submenu: Win32Menu?, fType: Int32) {
     self.title = title.LPCWSTR
     self.submenu = submenu
+
+    let imageHandle: BitmapHandle?
+    if let bitmap = image?.bitmap {
+      imageHandle = BitmapHandle(from: bitmap)
+    } else {
+      imageHandle = nil
+    }
+    self.image = imageHandle
+
     self.info = self.title.withUnsafeMutableBufferPointer {
       MENUITEMINFOW(cbSize: UINT(MemoryLayout<MENUITEMINFOW>.size),
-                    fMask: UINT(MIIM_FTYPE | MIIM_STATE | MIIM_ID | MIIM_STRING | MIIM_SUBMENU | MIIM_DATA),
+                    fMask: UINT(MIIM_FTYPE | MIIM_STATE | MIIM_ID | MIIM_STRING | MIIM_SUBMENU | MIIM_DATA | MIIM_BITMAP),
                     fType: UINT(fType),
                     fState: UINT(MFS_ENABLED),
                     wID: UInt32.random(in: .min ... .max),
@@ -91,25 +101,26 @@ internal class Win32MenuElement {
                     dwItemData: 0,
                     dwTypeData: $0.baseAddress,
                     cch: UINT(title.count),
-                    hbmpItem: nil)
+                    hbmpItem: imageHandle?.value)
     }
   }
 
   internal convenience init(_ element: MenuElement) {
     if let menu = element as? Menu {
       self.init(title: element.title,
+                image: element.image,
                 submenu: Win32Menu(MenuHandle(owning: CreatePopupMenu()),
                                    items: menu.children),
                 fType: MFT_STRING)
     } else {
-      self.init(title: element.title, submenu: nil, fType: MFT_STRING)
+      self.init(title: element.title, image: element.image, submenu: nil, fType: MFT_STRING)
     }
   }
 }
 
 extension Win32MenuElement {
   internal static var separator: Win32MenuElement {
-    Win32MenuElement(title: "", submenu: nil, fType: MFT_SEPARATOR)
+    Win32MenuElement(title: "", image: nil, submenu: nil, fType: MFT_SEPARATOR)
   }
 
   internal var isSeparator: Bool {
