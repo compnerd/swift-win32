3a3c4b87e738b4be97a062c64d07ec712490d60e
yostane <yostane@yahoo.fr>
2021-01-18 22:43:44 +0100

    HelloWorld example: added a title and a label (#377)
    HelloWorld: add a title and a label
    - The example doesn't show "hello world". This commit fixes this
    - This commit also shows how to add controls

--- Examples/HelloWorld/HelloWorld.swift
+++ Examples/HelloWorld/HelloWorld.swift
@@ -9,7 +9,7 @@ import SwiftWin32
 
 @main
 final class HelloWorld: ApplicationDelegate {
-  var window: Window?
+  var window: Window!
 }
 
 extension HelloWorld: SceneDelegate {
@@ -18,6 +18,19 @@ extension HelloWorld: SceneDelegate {
     guard let windowScene = scene as? WindowScene else { return }
 
     self.window = Window(windowScene: windowScene)
-    self.window?.makeKeyAndVisible()
+
+    // The rootViewController allows to set a title and add controls
+    self.window.rootViewController = ViewController()
+
+    // Add a title
+    self.window.rootViewController?.title = "swift-win32 Hello World"
+
+    // Add a hello world label
+    let label = Label(frame: Rect(x: 50.0, y: 100.0, width: 300.0, height: 30.0))
+    label.text = "Hello World"
+    self.window.addSubview(label)
+
+    // Show the window
+    self.window.makeKeyAndVisible()
   }
 }
