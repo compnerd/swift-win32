574c5518822ee9d0e8e8c0151c008503ae65affa
Saleem Abdulrasool <compnerd@compnerd.org>
2021-03-06 19:56:05 -0800

    tests: add some minor trivial tests
    Add some test cases for the `LayoutConstraint` interfaces.

--- Tests/AutoLayoutTests/AutoLayoutTests.swift
+++ Tests/AutoLayoutTests/AutoLayoutTests.swift
@@ -17,7 +17,35 @@ final class AutoLayoutTests: XCTestCase {
                                     multiplier: 1.0, constant: 0.0).isActive)
   }
 
+  func testLayoutConstraintActivate() {
+    let view: View = View(frame: .zero)
+    let container: View = View(frame: .zero)
+
+    let constraint: LayoutConstraint =
+        LayoutConstraint(item: view, attribute: .top, relatedBy: .equal,
+                         toItem: container, attribute: .top, multiplier: 1,
+                         constant: 100)
+    constraint.isActive = true
+    XCTAssertTrue(constraint.isActive)
+  }
+
+  func testLayoutConstraintActivateList() {
+    let view: View = View(frame: .zero)
+    let container: View = View(frame: .zero)
+
+    let constraints: [LayoutConstraint] = [
+      LayoutConstraint(item: view, attribute: .top, relatedBy: .equal,
+                        toItem: container, attribute: .top, multiplier: 1,
+                        constant: 100)
+    ]
+    LayoutConstraint.activate(constraints)
+
+    XCTAssertTrue(constraints[0].isActive)
+  }
+
   static var allTests = [
     ("testLayoutConstraintInactiveByDefault", testLayoutConstraintInactiveByDefault),
+    ("testLayoutConstraintActivate", testLayoutConstraintActivate),
+    ("testLayoutConstraintActivateList", testLayoutConstraintActivateList),
   ]
 }
