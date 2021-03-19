98c3bddf894b8c8e675bdca6c00daed218a8ffd6
Saleem Abdulrasool <compnerd@compnerd.org>
2021-03-18 21:44:29 -0700

    build: update the Swift install rule
    The Swift support in CMake has improved to the point where the only
    missing piece is the support for `install(TARGET)`.  Emulate the
    behaivour with `_install_target`.  Eventually, hopefully, this support
    can be replaced with `install(TARGET)`.  Use the 5.4 support for
    installing the module in the same manner on all targets.

--- cmake/Modules/SwiftSupport.cmake
+++ cmake/Modules/SwiftSupport.cmake
@@ -1,14 +1,11 @@
-
-include(CMakeParseArguments)
-
 # Returns the architecture name in a variable
 #
 # Usage:
-#   swift_get_host_arch(result_var_name)
+#   get_swift_host_arch(result_var_name)
 #
 # Sets ${result_var_name} with the converted architecture name derived from
 # CMAKE_SYSTEM_PROCESSOR.
-function(swift_get_host_arch result_var_name)
+function(get_swift_host_arch result_var_name)
   if("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "x86_64")
     set("${result_var_name}" "x86_64" PARENT_SCOPE)
   elseif("${CMAKE_SYSTEM_PROCESSOR}" STREQUAL "aarch64")
@@ -55,59 +52,34 @@ function(get_swift_host_os result_var_name)
   endif()
 endfunction()
 
-function(swift_install)
-  set(options)
-  set(single_parameter_options EXPORT)
-  set(multiple_parameter_options TARGETS)
-
-  cmake_parse_arguments(SI
-    "${options}"
-    "${single_parameter_options}"
-    "${multiple_parameter_options}"
-    ${ARGN})
-
-  list(LENGTH ${SI_TARGETS} si_num_targets)
-  if(si_num_targets GREATER 1)
-    message(SEND_ERROR "swift_install only supports a single target at a time")
-  endif()
-
+function(_install_target module)
   get_swift_host_os(swift_os)
-  get_target_property(type ${SI_TARGETS} TYPE)
+  get_target_property(type ${module} TYPE)
 
   if(type STREQUAL STATIC_LIBRARY)
-    set(swift_dir swift_static)
+    set(swift swift_static)
   else()
-    set(swift_dir swift)
+    set(swift swift)
   endif()
 
-  install(TARGETS ${SI_TARGETS}
-    EXPORT ${SI_EXPORT}
-    ARCHIVE DESTINATION lib/${swift_dir}/${swift_os}
-    LIBRARY DESTINATION lib/${swift_dir}/${swift_os}
+  install(TARGETS ${module}
+    ARCHIVE DESTINATION lib/${swift}/${swift_os}
+    LIBRARY DESTINATION lib/${swift}/${swift_os}
     RUNTIME DESTINATION bin)
   if(type STREQUAL EXECUTABLE)
     return()
   endif()
 
-  swift_get_host_arch(swift_arch)
-  get_target_property(module_name ${SI_TARGETS} Swift_MODULE_NAME)
+  get_swift_host_arch(swift_arch)
+  get_target_property(module_name ${module} Swift_MODULE_NAME)
   if(NOT module_name)
-    set(module_name ${SI_TARGETS})
+    set(module_name ${module})
   endif()
 
-  if(CMAKE_SYSTEM_NAME STREQUAL Darwin)
-    install(FILES
-      $<TARGET_PROPERTY:${SI_TARGETS},Swift_MODULE_DIRECTORY>/${module_name}.swiftdoc
-      DESTINATION lib/${swift_dir}/${swift_os}/${module_name}.swiftmodule
-      RENAME ${swift_arch}.swiftdoc)
-    install(FILES
-      $<TARGET_PROPERTY:${SI_TARGETS},Swift_MODULE_DIRECTORY>/${module_name}.swiftmodule
-      DESTINATION lib/${swift_dir}/${swift_os}/${module_name}.swiftmodule
-      RENAME ${swift_arch}.swiftmodule)
-  else()
-    install(FILES
-      $<TARGET_PROPERTY:${SI_TARGETS},Swift_MODULE_DIRECTORY>/${module_name}.swiftdoc
-      $<TARGET_PROPERTY:${SI_TARGETS},Swift_MODULE_DIRECTORY>/${module_name}.swiftmodule
-      DESTINATION lib/${swift_dir}/${swift_os}/${swift_arch})
-  endif()
+  install(FILES $<TARGET_PROPERTY:${module},Swift_MODULE_DIRECTORY>/${module_name}.swiftdoc
+    DESTINATION lib/${swift}/${swift_os}/${module_name}.swiftmodule
+    RENAME ${swift_arch}.swiftdoc)
+  install(FILES $<TARGET_PROPERTY:${module},Swift_MODULE_DIRECTORY>/${module_name}.swiftmodule
+    DESTINATION lib/${swift}/${swift_os}/${module_name}.swiftmodule
+    RENAME ${swift_arch}.swiftmodule)
 endfunction()
