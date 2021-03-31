f5818c1470251896a0b1d2f70b6bbf7ccfcf3994
Ryan Allan <ryan.allan@mts.com>
2021-03-30 23:28:43 -0400

    Add click dispatch to label

--- Sources/SwiftWin32/Views and Controls/Label.swift
+++ Sources/SwiftWin32/Views and Controls/Label.swift
@@ -7,9 +7,22 @@
 
 import WinSDK
 
+private let SwiftLabelProc: SUBCLASSPROC = { (hWnd, uMsg, wParam, lParam, uIdSubclass, dwRefData) in
+  let label: Label? = unsafeBitCast(dwRefData, to: AnyObject.self) as? Label
+
+  switch uMsg {
+  case UINT(WM_LBUTTONUP):
+    label?.sendActions(for: .primaryActionTriggered)
+  default:
+    break
+  }
+
+  return DefSubclassProc(hWnd, uMsg, wParam, lParam)
+}
+
 public class Label: Control {
   private static let `class`: WindowClass = WindowClass(named: WC_STATIC)
-  private static let style: WindowStyle = (base: WS_TABSTOP, extended: 0)
+  private static let style: WindowStyle = (base: WS_TABSTOP | DWORD(SS_NOTIFY), extended: 0)
 
   public override var font: Font! {
     get { return super.font }
@@ -21,6 +34,8 @@ public class Label: Control {
 
   public init(frame: Rect) {
     super.init(frame: frame, class: Label.class, style: Label.style)
+    _ = SetWindowSubclass(hWnd, SwiftLabelProc, UINT_PTR(1),
+                         unsafeBitCast(self as AnyObject, to: DWORD_PTR.self))
   }
 
   // ContentSizeCategoryAdjusting
