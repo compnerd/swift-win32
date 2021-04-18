b46e4678bb9179e53cdb6f7ad992992c1d30bfec
Saleem Abdulrasool <compnerd@compnerd.org>
2021-04-18 15:07:33 -0700

    SwiftUI: add initial definition for Group
    This adds the Group type and add a couple of associated extensions for
    construction via `SceneBuilder`s.

---
+++ Sources/SwiftWin32UI/View Layout and Presentation/Group.swift
@@ -0,0 +1,39 @@
+/**
+ * Copyright © 2020 Saleem Abdulrasool <compnerd@compnerd.org>
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ **/
+
+/// An affordance for grouping view content.
+@frozen
+public struct Group<Content> {
+  public typealias Body = Never
+
+  @usableFromInline
+  internal var content: Content
+}
+
+// MARK - Creating a Group
+
+extension Group where Content: View {
+  /// Available when `Content` conforms to `View`.
+  @inlinable
+  public init(@ViewBuilder content: () -> Content) {
+    self.content = content()
+  }
+}
+
+extension Group where Content: Scene {
+  /// Available when `Content` conforms to `Scene`.
+  @inlinable
+  public init(@SceneBuilder content: () -> Content) {
+    self.content = content()
+  }
+}
+
+extension Group: CustomStringConvertible {
+  public var description: String {
+    "<Group: \(content)>"
+  }
+}
