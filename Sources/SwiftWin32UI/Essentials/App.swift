a0c6167335c165f319e36f07e41323936c25aabe
Saleem Abdulrasool <compnerd@compnerd.org>
2021-04-18 14:25:38 -0700

    SwiftUI: rename `Application` to `App`
    This matches the SwiftUI model more accurately.

--- Sources/SwiftWin32UI/Essentials/App.swift
+++ Sources/SwiftWin32UI/Essentials/App.swift
@@ -8,8 +8,8 @@
 import SwiftWin32
 
 /// A type that represents the structure and behaviour of an application.
-public protocol Application {
-  // MARK - Implementing an Application
+public protocol App {
+  // MARK - Implementing an App
 
   /// The type of scene representing the content of the application.
   associatedtype Body: Scene
@@ -24,7 +24,7 @@ public protocol Application {
   init()
 }
 
-extension Application {
+extension App {
   /// Initializes and runs the application.
   public static func main() {
     ApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil,
