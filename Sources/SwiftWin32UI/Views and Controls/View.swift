673159a9943ee7c7d851b9eb8b0e37ccd39f68e8
Saleem Abdulrasool <compnerd@compnerd.org>
2021-04-21 19:05:05 -0700

    SwiftUI: add some doc-comments to View

--- Sources/SwiftWin32UI/Views and Controls/View.swift
+++ Sources/SwiftWin32UI/Views and Controls/View.swift
@@ -5,9 +5,13 @@
  * SPDX-License-Identifier: BSD-3-Clause
  **/
 
+/// A type that represents part of your app’s user interface and provides
+/// modifiers that you use to configure views.
 public protocol View {
+  /// The type of view representing the body of this view.
   associatedtype Body: View
 
+  /// The content and behavior of the view.
   @ViewBuilder
   var body: Self.Body { get }
 }
