ea5fa0cad55bb6556b1468dd3f4400dc8144e811
Ryan Allan <ryan.allan@mts.com>
2021-05-06 00:08:37 -0400

    Fix applying() function on Rect

--- Tests/CoreGraphicsTests/CoreGraphicsTests.swift
+++ Tests/CoreGraphicsTests/CoreGraphicsTests.swift
@@ -37,10 +37,45 @@ final class CoreGraphicsTests: XCTestCase {
                         size: Size(width: 16, height: 16))
     XCTAssertEqual(r2.midX, 12)
     XCTAssertEqual(r2.midY, 12)
+
+  }
+
+  func testRectApplyAffineTransform() {
+    var rect: Rect =
+        Rect(origin: Point(x: 45, y: 115),
+             size: Size(width: 13, height: 14))
+            .applying(AffineTransform(rotationAngle: 42))
+
+    // Test data comes from the UIKit implementation, running on an iPhone,
+    // and so deviation is expected.
+    var accuracy = 1e-13
+                     
+    XCTAssertEqual(rect.origin.x, 82.20082974097352, accuracy: accuracy)
+    XCTAssertEqual(rect.origin.y, -104.75635541260408, accuracy: accuracy)
+    XCTAssertEqual(rect.size.width, 18.031110765667435, accuracy: accuracy)
+    XCTAssertEqual(rect.size.height, 17.514574532740156, accuracy: accuracy)
+
+    // Deviation increases with more operations
+    accuracy = 1e-12
+
+    rect =
+        Rect(origin: Point(x: 45, y: 115),
+             size: Size(width: 13, height: 14))
+            .applying(AffineTransform(rotationAngle: -104))
+            .applying(AffineTransform(translationX: -26, y: 22))
+            .applying(AffineTransform(scaleX: 7, y: 5))
+
+    XCTAssertEqual(rect.origin.x, -856.8534424207578, accuracy: accuracy)
+    XCTAssertEqual(rect.origin.y, -428.36482622296273, accuracy: accuracy)
+    XCTAssertEqual(rect.size.width, 117.68398448828839, accuracy: accuracy)
+    XCTAssertEqual(rect.size.height, 87.18621695814943, accuracy: accuracy)
+
   }
 
   static var allTests = [
     ("testAffineTransformIdentity", testAffineTransformIdentity),
-    ("testAffineTransformIdentityIsIdentity", testAffineTransformIdentityIsIdentity)
+    ("testAffineTransformIdentityIsIdentity", testAffineTransformIdentityIsIdentity),
+    ("testRectComputedProperties", testRectComputedProperties),
+    ("testRectApplyAffineTransform", testRectApplyAffineTransform),
   ]
 }
