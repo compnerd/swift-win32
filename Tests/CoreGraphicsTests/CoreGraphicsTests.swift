57ec6fc5fb7204d4e381072b3e6ca8ecb8229cc9
Ryan Allan <ryan.allan@mts.com>
2021-05-14 16:39:46 -0400

    Rework == operator on Rect to take standardization into account, isNull() requires special handler

--- Tests/CoreGraphicsTests/CoreGraphicsTests.swift
+++ Tests/CoreGraphicsTests/CoreGraphicsTests.swift
@@ -203,6 +203,19 @@ final class CoreGraphicsTests: XCTestCase {
     XCTAssertFalse(r4.intersects(r1))
   }
 
+  func testRectNonstandardEquality() {
+    let r1: Rect = Rect(x: 0, y: 0, width: 10, height: 10)
+    let r2: Rect = Rect(x: 10, y: 10, width: -10, height: -10)
+    let r3: Rect = Rect(x: 10, y: 10, width: -9.9, height: -10)
+
+    XCTAssertTrue(r1 == r2)
+    XCTAssertFalse(r2 == r3)
+
+    let null1: Rect = Rect.null
+    let null2: Rect = Rect.null
+    XCTAssertTrue(null1 == null2)
+  }
+
   static var allTests = [
     ("testAffineTransformIdentity", testAffineTransformIdentity),
     ("testAffineTransformIdentityIsIdentity", testAffineTransformIdentityIsIdentity),
@@ -216,5 +229,6 @@ final class CoreGraphicsTests: XCTestCase {
     ("testRectInsetBy", testRectInsetBy),
     ("testRectIntersection", testRectIntersection),
     ("testRectIntersects", testRectIntersects),
+    ("testRectNonstandardEquality", testRectNonstandardEquality),
   ]
 }
