888aa388ede649b01cd98651fe29a6c5c33c3f59
Tomasz Pieczykolan <t.pieczykolan@gmail.com>
2021-05-18 17:21:21 +0200

    Add an empty test class for ViewController

---
+++ Tests/UICoreTests/ViewControllerTests.swift
@@ -0,0 +1,8 @@
+// Copyright © 2021 Saleem Abdulrasool <compnerd@compnerd.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+import XCTest
+@testable import SwiftWin32
+
+final class ViewControllerTests: XCTestCase {
+}
