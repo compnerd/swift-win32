75ee9516c15f43a07ecc427eeff090e08b9d370e
Tomasz Pieczykolan <t.pieczykolan@gmail.com>
2021-05-18 17:31:08 +0200

    Add tests for loading the view of ViewController

--- Tests/UICoreTests/ViewControllerTests.swift
+++ Tests/UICoreTests/ViewControllerTests.swift
@@ -5,4 +5,27 @@ import XCTest
 @testable import SwiftWin32
 
 final class ViewControllerTests: XCTestCase {
+  func testLazyViewLoading() {
+    let sut = ViewController()
+
+    XCTAssertNil(sut.viewIfLoaded)
+    XCTAssertFalse(sut.isViewLoaded)
+
+    _ = sut.view
+
+    XCTAssertNotNil(sut.viewIfLoaded)
+    XCTAssertTrue(sut.isViewLoaded)
+  }
+
+  func testManualViewLoading() {
+    let sut = ViewController()
+
+    XCTAssertNil(sut.viewIfLoaded)
+    XCTAssertFalse(sut.isViewLoaded)
+
+    sut.loadViewIfNeeded()
+
+    XCTAssertNotNil(sut.viewIfLoaded)
+    XCTAssertTrue(sut.isViewLoaded)
+  }
 }
