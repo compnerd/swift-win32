9b85b96448c02f4bd735bbca170e8f1c39ce5537
Saleem Abdulrasool <compnerd@compnerd.org>
2021-06-02 09:15:55 -0700

    SwiftUI: add definition of EquatableView

---
+++ Sources/SwiftWin32UI/View Layout and Presentation/EquatableView.swift
@@ -0,0 +1,18 @@
+// Copyright © 2021 Saleem Abdulrasool <compnerd@compnerd.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+/// A view type that compares itself against its previous value and prevents its
+/// child updating if its new value is the same as its old value.
+@frozen
+public struct EquatableView<Content: View & Equatable>: View {
+  public typealias Body = Never
+
+  // MARK - Creating an Equatable View
+
+  public var content: Content
+
+  @inlinable
+  public init(content: Content) {
+    self.content = content
+  }
+}
