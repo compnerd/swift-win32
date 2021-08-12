b1430415095629c1d07cf8b227b8ab310348ef44
Saleem Abdulrasool <compnerd@compnerd.org>
2021-08-11 17:45:24 -0700

    App and Environment: improve `TraitCollection`
    Add some documentation comments and add some additional interfaces for
    `TraitCollection`.

--- Sources/SwiftWin32/App and Environment/TraitCollection.swift
+++ Sources/SwiftWin32/App and Environment/TraitCollection.swift
@@ -101,32 +101,57 @@ public struct ContentSizeCategory: Equatable, Hashable, RawRepresentable {
 }
 
 extension ContentSizeCategory {
-  public static let unspecified: ContentSizeCategory =
-      ContentSizeCategory(rawValue: "_UICTContentSizeCategoryUnspecified")
-  public static let extraSmall: ContentSizeCategory =
-      ContentSizeCategory(rawValue: "UICTContentSizeCategoryXS")
-  public static let small: ContentSizeCategory =
-      ContentSizeCategory(rawValue: "UICTContentSizeCategoryS")
-  public static let medium: ContentSizeCategory =
-      ContentSizeCategory(rawValue: "UICTContentSizeCategoryM")
-  public static let large: ContentSizeCategory =
-      ContentSizeCategory(rawValue: "UICTContentSizeCategoryL")
-  public static let extraLarge: ContentSizeCategory =
-      ContentSizeCategory(rawValue: "UICTContentSizeCategoryXL")
-  public static let extraExtraLarge: ContentSizeCategory =
-      ContentSizeCategory(rawValue: "UICTContentSizeCategoryXXL")
-  public static let extraExtraExtraLarge: ContentSizeCategory =
-      ContentSizeCategory(rawValue: "UICTContentSizeCategoryXXXL")
-  public static let accessibilityMedium: ContentSizeCategory =
-      ContentSizeCategory(rawValue: "UICTContentSizeCategoryAccessibilityM")
-  public static let accessibilityLarge: ContentSizeCategory =
-      ContentSizeCategory(rawValue: "UICTContentSizeCategoryAccessibilityL")
-  public static let accessibilityExtraLarge: ContentSizeCategory =
-      ContentSizeCategory(rawValue: "UICTContentSizeCategoryAccessibilityXL")
-  public static let accessibilityExtraExtraLarge: ContentSizeCategory =
-      ContentSizeCategory(rawValue: "UICTContentSizeCategoryAccessibilityXXL")
-  public static let accessibilityExtraExtraExtraLarge: ContentSizeCategory =
-      ContentSizeCategory(rawValue: "UICTContentSizeCategoryAccessibilityXXXL")
+  public static var unspecified: ContentSizeCategory {
+    ContentSizeCategory(rawValue: "_UICTContentSizeCategoryUnspecified")
+  }
+
+  public static var extraSmall: ContentSizeCategory {
+    ContentSizeCategory(rawValue: "UICTContentSizeCategoryXS")
+  }
+
+  public static var small: ContentSizeCategory {
+    ContentSizeCategory(rawValue: "UICTContentSizeCategoryS")
+  }
+
+  public static var medium: ContentSizeCategory {
+    ContentSizeCategory(rawValue: "UICTContentSizeCategoryM")
+  }
+
+  public static var large: ContentSizeCategory {
+    ContentSizeCategory(rawValue: "UICTContentSizeCategoryL")
+  }
+
+  public static var extraLarge: ContentSizeCategory {
+    ContentSizeCategory(rawValue: "UICTContentSizeCategoryXL")
+  }
+
+  public static var extraExtraLarge: ContentSizeCategory {
+    ContentSizeCategory(rawValue: "UICTContentSizeCategoryXXL")
+  }
+
+  public static var extraExtraExtraLarge: ContentSizeCategory {
+    ContentSizeCategory(rawValue: "UICTContentSizeCategoryXXXL")
+  }
+
+  public static var accessibilityMedium: ContentSizeCategory {
+    ContentSizeCategory(rawValue: "UICTContentSizeCategoryAccessibilityM")
+  }
+
+  public static var accessibilityLarge: ContentSizeCategory {
+    ContentSizeCategory(rawValue: "UICTContentSizeCategoryAccessibilityL")
+  }
+
+  public static var accessibilityExtraLarge: ContentSizeCategory {
+    ContentSizeCategory(rawValue: "UICTContentSizeCategoryAccessibilityXL")
+  }
+
+  public static var accessibilityExtraExtraLarge: ContentSizeCategory {
+    ContentSizeCategory(rawValue: "UICTContentSizeCategoryAccessibilityXXL")
+  }
+
+  public static var accessibilityExtraExtraExtraLarge: ContentSizeCategory {
+    ContentSizeCategory(rawValue: "UICTContentSizeCategoryAccessibilityXXXL")
+  }
 }
 
 private func GetCurrentColorScheme() -> UserInterfaceStyle {
@@ -195,55 +220,23 @@ private func GetCurrentDisplayGamut() -> DisplayGamut {
 }
 
 public class TraitCollection {
-  public private(set) static var current: TraitCollection =
-      TraitCollection(traitsFrom: [
-        TraitCollection(accessibilityContrast: GetCurrentAccessibilityContrast()),
-        TraitCollection(displayGamut: GetCurrentDisplayGamut()),
-        TraitCollection(displayScale: GetCurrentDisplayScale()),
-        TraitCollection(layoutDirection: GetCurrentLayoutDirection()),
-        TraitCollection(preferredContentSizeCategory: .large),
-        TraitCollection(userInterfaceIdiom: GetCurrentDeviceFamily()),
-        TraitCollection(userInterfaceLevel: .base),
-        TraitCollection(userInterfaceStyle: GetCurrentColorScheme()),
+  // MARK -  Creating a Trait Collection
 
-        TraitCollection(horizontalSizeClass: Device.current.isPortrait
-                                                ? .compact
-                                                : .regular),
-        TraitCollection(verticalSizeClass: Device.current.isPortrait
-                                              ? .regular
-                                              : .compact),
-      ])
-
-  // Retriving Size Class Traits
-  public private(set) var horizontalSizeClass: UserInterfaceSizeClass =
-      .unspecified
-  public private(set) var verticalSizeClass: UserInterfaceSizeClass =
-      .unspecified
-
-  // Retriving Display-Related Traits
-  public private(set) var displayScale: Double = 0.0
-  public private(set) var displayGamut: DisplayGamut = .unspecified
-
-  // Retriving Interface-Related Traits
-  public private(set) var userInterfaceStyle: UserInterfaceStyle = .unspecified
-  public private(set) var userInterfaceIdiom: UserInterfaceIdiom = .unspecified
-  public private(set) var userInterfaceLevel: UserInterfaceLevel = .unspecified
-  public private(set) var layoutDirection: TraitEnvironmentLayoutDirection =
-      .unspecified
-  public private(set) var accessibilityContrast: AccessibilityContrast =
-      .unspecified
-  public private(set) var legibilityWeight: LegibilityWeight = .unspecified
-  public private(set) var activeAppearance: UserInterfaceActiveAppearance =
-      .unspecified
-
-  // Retriving the Force Touch Capability
-  public private(set) var forceTouchCapability: ForceTouchCapability =
-      .unspecified
-
-  // Retriving Content Size Category Information
-  public private(set) var preferredContentSizeCategory: ContentSizeCategory =
-      .unspecified
-
+  /// Creates a trait collection that consists of traits merged from a specified
+  /// array of trait collections.
+  ///
+  /// This method takes an array of one or more trait collections and merges
+  /// them to create a new trait collection. If the array contains more than one
+  /// element, the highest-indexed element that contains a given trait is used
+  /// for that trait. For example, the following code snippet creates a trait
+  /// collection with a compact horizontal size class, because the second
+  /// element in the array overrides the first for that trait:
+  ///
+  /// ```swift
+  /// let collection1: TraitCollection = TraitCollection(horizontalSizeClass: .regular)
+  /// let collection2: TraitCollection = TraitCollection(horizontalSizeClass: .compact)
+  /// let collection3: TraitCollection = TraitCollection(traitsFrom: [collection1, collection2])
+  /// ```
   public init(traitsFrom traits: [TraitCollection]) {
     _ = traits.map {
       if $0.horizontalSizeClass != .unspecified {
@@ -288,55 +281,266 @@ public class TraitCollection {
     }
   }
 
-  public init(userInterfaceStyle: UserInterfaceStyle) {
-    self.userInterfaceStyle = userInterfaceStyle
-  }
-
+  /// Creates a trait collection that contains only a specified interface idiom.
   public init(userInterfaceIdiom: UserInterfaceIdiom) {
     self.userInterfaceIdiom = userInterfaceIdiom
   }
 
+  /// Creates a trait collection that contains only a specified horizontal size
+  /// class.
   public init(horizontalSizeClass: UserInterfaceSizeClass) {
     self.horizontalSizeClass = horizontalSizeClass
   }
 
+  /// Creates a trait collection that contains only a specified vertical size
+  /// class.
   public init(verticalSizeClass: UserInterfaceSizeClass) {
     self.verticalSizeClass = verticalSizeClass
   }
 
+  /// Creates a trait collection that contains only the specified user interface
+  /// style trait.
+  public init(userInterfaceStyle: UserInterfaceStyle) {
+    self.userInterfaceStyle = userInterfaceStyle
+  }
+
+  /// Creates a trait collection that contains only the specified accessibility
+  /// contrast trait.
   public init(accessibilityContrast: AccessibilityContrast) {
     self.accessibilityContrast = accessibilityContrast
   }
 
+  /// Creates a trait collection that contains only the specified user interface
+  /// level trait.
   public init(userInterfaceLevel: UserInterfaceLevel) {
     self.userInterfaceLevel = userInterfaceLevel
   }
 
+  /// Creates a trait collection that contains only the specified legibility
+  /// weight trait.
   public init(legibilityWeight: LegibilityWeight) {
     self.legibilityWeight = legibilityWeight
   }
 
-  public init(activeAppearance: UserInterfaceActiveAppearance) {
-    self.activeAppearance = activeAppearance
-  }
-
+  /// Creates a trait collection that contains only a specified force touch
+  /// capability trait.
   public init(forceTouchCapability: ForceTouchCapability) {
     self.forceTouchCapability = forceTouchCapability
   }
 
+  /// Creates a trait collection that contains only a specified display scale.
   public init(displayScale: Double) {
     self.displayScale = displayScale
   }
 
+  /// Creates a trait collection that contains only the specified display gamut
+  /// trait.
   public init(displayGamut: DisplayGamut) {
     self.displayGamut = displayGamut
   }
 
+  /// Creates a trait collection that contains only the specified layout
+  /// direction trait.
   public init(layoutDirection: TraitEnvironmentLayoutDirection) {
     self.layoutDirection = layoutDirection
   }
 
+  /// Creates a trait collection that contains only the specified content size
+  /// category trait.
   public init(preferredContentSizeCategory: ContentSizeCategory) {
     self.preferredContentSizeCategory = preferredContentSizeCategory
+  }
+
+  /// Creates a trait collection that contains only the specified active
+  /// appearance trait.
+  public init(activeAppearance: UserInterfaceActiveAppearance) {
+    self.activeAppearance = activeAppearance
+  }
+
+  // MARK - Getting the Current Traits
+
+  // TODO(compnerd) The framework stores the value for this property as a
+  // thread-local variable, so access to it is fast.
+
+  /// The complete set of traits for the current environment.
+  ///
+  /// The framework updates the value of this property before calling several
+  /// well-known methods of `View`, `ViewController`, and
+  /// `PresentationController`. The trait collection contains a complete set of
+  /// trait values describing the current environment, and does not include
+  /// unspecified or unknown values.
+  ///
+  /// Changing the traits on a nonmain thread does not affect the current traits
+  /// on your applications's main thread.
+  public private(set) static var current: TraitCollection =
+      TraitCollection(traitsFrom: [
+        TraitCollection(accessibilityContrast: GetCurrentAccessibilityContrast()),
+        TraitCollection(displayGamut: GetCurrentDisplayGamut()),
+        TraitCollection(displayScale: GetCurrentDisplayScale()),
+        TraitCollection(layoutDirection: GetCurrentLayoutDirection()),
+        TraitCollection(preferredContentSizeCategory: .large),
+        TraitCollection(userInterfaceIdiom: GetCurrentDeviceFamily()),
+        TraitCollection(userInterfaceLevel: .base),
+        TraitCollection(userInterfaceStyle: GetCurrentColorScheme()),
+
+        TraitCollection(horizontalSizeClass: Device.current.isPortrait
+                                                ? .compact
+                                                : .regular),
+        TraitCollection(verticalSizeClass: Device.current.isPortrait
+                                              ? .regular
+                                              : .compact),
+      ])
+
+  // MARK - Retriving Size Class Traits
+
+  /// The horizontal size class of the trait collection.
+  public private(set) var horizontalSizeClass: UserInterfaceSizeClass =
+      .unspecified
+
+  /// The vertical size class of the trait collection.
+  public private(set) var verticalSizeClass: UserInterfaceSizeClass =
+      .unspecified
+
+  // MARK -Retriving Display-Related Traits
+
+  /// The display scale of the trait collection.
+  ///
+  /// A value of 1.0 indicates a non-Retina display and a value of 2.0 indicates
+  /// a Retina display. The default display scale for a trait collection is 0.0
+  /// (indicating unspecified).
+  public private(set) var displayScale: Double = 0.0
+
+  /// The gamut of the current display.
+  public private(set) var displayGamut: DisplayGamut = .unspecified
+
+  // MARK - Retriving Interface-Related Traits
+
+  /// The style associated with the user interface.
+  ///
+  /// Use this trait to determine whether your interface should be configured
+  /// with a dark or light appearance. The default value of this trait is set to
+  /// the corresponding appearance setting on the user's device.
+  public private(set) var userInterfaceStyle: UserInterfaceStyle = .unspecified
+
+  /// The user interface idiom of the trait collection.
+  ///
+  /// The default user interface idiom for a trait collection is
+  /// `UserInterfaceIdiom.unspecified`.
+  public private(set) var userInterfaceIdiom: UserInterfaceIdiom = .unspecified
+
+  /// The elevation level of the interface.
+  ///
+  /// Levels create a visual separation between different parts of your UI.
+  /// `Window` content typically appears at the `UserInterfaceLevel.base` level.
+  /// When you want parts of your UI to stand out from the underlying
+  /// background, assign the `UserInterfaceLevel.elevated` level to them. For
+  /// example, the system assigns the `UserInterfaceLevel.elevated` level to
+  /// alerts and popovers.
+  public private(set) var userInterfaceLevel: UserInterfaceLevel = .unspecified
+
+  /// The layout direction associated with the current environment.
+  ///
+  /// Use this trait to determine whether the underlying environment uses a
+  /// left-to-right or right-to-left orientation.
+  public private(set) var layoutDirection: TraitEnvironmentLayoutDirection =
+      .unspecified
+
+  /// The accessibility contrast associated with the current environment.
+  ///
+  /// Use this trait to determine whether the user requested a high contrast
+  /// between foreground and background content. The user sets the contrast
+  /// level in Accessibility Settings.
+  public private(set) var accessibilityContrast: AccessibilityContrast =
+      .unspecified
+
+  /// The font weight to apply to text.
+  ///
+  /// The legibility weight reflects the value of the Bold Text display setting.
+  public private(set) var legibilityWeight: LegibilityWeight = .unspecified
+
+  /// A property that indicates whether the user interface has an active
+  /// appearance.
+  ///
+  /// The active appearance varies according to window activation state.
+  public private(set) var activeAppearance: UserInterfaceActiveAppearance =
+      .unspecified
+
+  // MARK - Retriving the Force Touch Capability
+
+  /// The force touch capability value of the trait collection.
+  ///
+  /// Force Touch is available only on certain devices. On those devices,
+  /// availability is determined by the user's associated accessibility setting
+  /// in Settings. Check this property's value on app launch, and in your
+  /// implementation of the `traitCollectionDidChange(_:)` method.
+  ///
+  /// If this property does not contain a value, the meaning is equivalent to
+  /// the value `ForceTouchCapability.unknown`.
+  public private(set) var forceTouchCapability: ForceTouchCapability =
+      .unspecified
+
+  // MARK - Retriving Content Size Category Information
+
+  /// The font sizing option preferred by the user.
+  ///
+  /// With dynamic text sizing, users can ask that apps display text using fonts
+  /// that are larger or smaller than the normal font size defined by the
+  /// system. For example, a user with a visual impairment might request a
+  /// larger default font size to make it easier to read text. Use the value of
+  /// this property to request a `Font` object that matches the user's requested
+  /// size.
+  public private(set) var preferredContentSizeCategory: ContentSizeCategory =
+      .unspecified
+
+  // MARK - Comparing Trait Collections
+
+  /// Queries whether a trait collection contains all of another trait
+  /// collection's values.
+  ///
+  /// Use this method to compare two standalone trait collections, or to compare
+  /// the interface environment's trait collection to a standalone trait
+  /// collection.
+  public func containsTraits(in trait: TraitCollection?) -> Bool {
+    fatalError("\(#function) not yet implemented")
+  }
+
+  /// Queries whether changing between the specified and current trait
+  /// collections would affect color values.
+  ///
+  /// Use this method to determine whether changing the traits of the current
+  /// environment would also change the colors in your interface. For example,
+  /// changing the `userInterfaceStyle` or `accessibilityContrast` property
+  /// usually changes the colors of your interface.
+  public func hasDifferentColorAppearance(comparedTo traitCollection: TraitCollection?)
+      -> Bool {
+    fatalError("\(#function) not yet implemented")
+  }
+
+
+  // MARK - Getting an Image Configuration Object
+
+  /// An image configuration object compatible with this trait collection.
+  ///
+  /// The `Image.Configuration` object in this property contains the same traits
+  /// as the current trait collection.
+  public private(set) var imageConfiguration: Image.Configuration {
+    fatalError("\(#function) not yet implemented")
+  }
+
+  // MARK - Performing Actions with the Current Traits
+
+  /// Executes custom code using the traits of the current trait collection.
+  ///
+  /// Use this method when you want to execute code using a set of traits that
+  /// differ from those in the current trait environment. For example, you might
+  /// use this method to draw part of a view's content with a different set of
+  /// traits. This method temporarily replaces the traits of the current
+  /// environment with the ones found in the current `TraitCollection`. After
+  /// the actions block finishes, the method restores the original traits to the
+  /// environment.
+  ///
+  /// You can call this method from any thread of your application.
+  public func performAsCurrent(_ actions: () -> Void) {
+    fatalError("\(#function) not yet implemented")
   }
 }
