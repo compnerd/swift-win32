9231503400a7a9a840fae2ff0c045957746247b7
Saleem Abdulrasool <compnerd@compnerd.org>
2023-08-06 20:33:56 -0700

    SwiftWin32: add `Label.textAlignment` property
    Add the missing `textAlignment` property for the partial cases which are
    trivial to map.  This allows for better control over the rendering of
    the text.

---
+++ Sources/SwiftWin32/Views and Controls/TextAlignment.swift
@@ -0,0 +1,16 @@
+// Copyright © 2023 Saleem Abdulrasool <compnerd@compnerd.org>
+// SPDX-License-Identifier: BSD-3-Clause
+
+/// Constants that specify text alignment.
+public enum TextAlignment: Int {
+  /// Text uses the default alignment for the current localization of the app.
+  case natural
+  /// Text is left-aligned.
+  case left
+  /// Text is right-aligned.
+  case right
+  /// Text is center-aligned.
+  case center
+  /// Text is justified.
+  case justified
+}
