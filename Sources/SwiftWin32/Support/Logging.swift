bf8b09cba530ddc939f76ed2218a91d9b17a6547
Saleem Abdulrasool <compnerd@compnerd.org>
2023-11-03 19:59:08 -0700

    SwiftWin32: silence warning on recent development compilers
    Use the newly minted support for access control on imports (SE-0409) to
    clean up some newer warnings.

--- Sources/SwiftWin32/Support/Logging.swift
+++ Sources/SwiftWin32/Support/Logging.swift
@@ -1,7 +1,11 @@
 // Copyright © 2020 Saleem Abdulrasool <compnerd@compnerd.org>
 // SPDX-License-Identifier: BSD-3-Clause
 
+#if swift(>=5.9)
+internal import Logging
+#else
 @_implementationOnly
 import Logging
+#endif
 
 internal let log: Logger = Logger(label: "org.compnerd.swift-win32")
